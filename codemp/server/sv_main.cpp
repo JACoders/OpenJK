8f90d8d34645507fe03b0d3aa1ee38098b4f5165
Ensiform <ensiform@gmail.com>
2013-04-05 18:10:43 -0500

    Fixed rcon buffer length being really large

--- codemp/server/sv_main.cpp
+++ codemp/server/sv_main.cpp
@@ -674,7 +674,7 @@ void SVC_RemoteCommand( netadr_t from, msg_t *msg ) {
 	char		remaining[1024];
 	// TTimo - scaled down to accumulate, but not overflow anything network wise, print wise etc.
 	// (OOB messages are the bottleneck here)
-#define	SV_OUTPUTBUF_LENGTH	(MAX_MSGLEN - 16)
+#define	SV_OUTPUTBUF_LENGTH	(1024 - 16)
 	char		sv_outputbuf[SV_OUTPUTBUF_LENGTH];
 	char		*cmd_aux;
 
