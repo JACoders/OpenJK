340ee99ccb078de609460bdb95e5fec3974aef90
Ensiform <ensiform@gmail.com>
2013-04-05 09:49:52 -0500

    Fixed external console printer with skipnotify.
    Added checks for skipnotify/* in the external console printer.

--- codemp/win32/win_main.cpp
+++ codemp/win32/win_main.cpp
@@ -117,6 +117,14 @@ Sys_Print
 ==============
 */
 void Sys_Print( const char *msg ) {
+	// TTimo - prefix for text that shows up in console but not in notify
+	// backported from RTCW
+	if ( !Q_strncmp( msg, "[skipnotify]", 12 ) ) {
+		msg += 12;
+	}
+	if ( msg[0] == '*' ) {
+		msg += 1;
+	}
 	Conbuf_AppendText( msg );
 }
 
