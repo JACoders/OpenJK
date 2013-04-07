5a6c68e4673b77ce862645ea62429186d5f1a8db
Ensiform <ensiform@gmail.com>
2013-04-06 21:37:45 -0500

    Fixed rate limiting code (due to NA_BOT being 0 rather than NA_BAD)

--- codemp/qcommon/qcommon.h
+++ codemp/qcommon/qcommon.h
@@ -102,8 +102,8 @@ NET
 #define	MAX_RELIABLE_COMMANDS	128			// max string commands buffered for restransmit
 
 typedef enum {
+	NA_BAD = 0,					// an address lookup failed
 	NA_BOT,
-	NA_BAD,					// an address lookup failed
 	NA_LOOPBACK,
 	NA_BROADCAST,
 	NA_IP,
