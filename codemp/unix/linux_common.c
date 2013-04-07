92891bb80e73f4bf15dbc42ddb8039c0d1496d92
Axel Isouard <axel.isouard@anexfi.fr>
2013-04-07 03:36:25 +0200

    unix compatibility backup

--- codemp/unix/linux_common.c
+++ codemp/unix/linux_common.c
@@ -14,13 +14,13 @@
 
 // bk001207 - we need something under Linux, too. Mac?
 #if 1 // defined(C_ONLY) // bk010102 - dedicated?
-void Com_Memcpy (void* dest, const void* src, const size_t count) {
+/*void Com_Memcpy (void* dest, const void* src, const size_t count) {
   memcpy(dest, src, count);
 }
 
 void Com_Memset (void* dest, const int val, const size_t count) {
   memset(dest, val, count);
-}
+}*/
 
 #else
 
