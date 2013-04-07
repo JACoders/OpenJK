dc5e2ea1f469874a44e103db5821b19ec60b63c9
Nick Whitlock <eezstreet@live.com>
2013-04-07 17:38:12 -0400

    Added fontlist command from MP. Added shift+esc alias for console. Removed a useless variable in cg_text.cpp

--- code/renderer/tr_init.cpp
+++ code/renderer/tr_init.cpp
@@ -1279,6 +1279,7 @@ extern qboolean Sys_LowPhysicalMemory();
 	Cmd_AddCommand( "imagelist", R_ImageList_f );
 	Cmd_AddCommand( "shaderlist", R_ShaderList_f );
 	Cmd_AddCommand( "skinlist", R_SkinList_f );
+	Cmd_AddCommand( "fontlist", R_FontList_f );
 	Cmd_AddCommand( "modellist", R_Modellist_f );
 #ifndef _XBOX
 	Cmd_AddCommand( "modelist", R_ModeList_f );
@@ -1429,6 +1430,7 @@ void RE_Shutdown( qboolean destroyWindow ) {
 	Cmd_RemoveCommand ("imagelist");
 	Cmd_RemoveCommand ("shaderlist");
 	Cmd_RemoveCommand ("skinlist");
+	Cmd_RemoveCommand ("fontlist");
 	Cmd_RemoveCommand ("modellist");
 #ifndef _XBOX
 	Cmd_RemoveCommand ("modelist" );
