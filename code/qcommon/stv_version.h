d1dd82422b63751fb473cbd19dcf1215cb5b91e3
eezstreet <eezstreet@live.com>
2013-04-07 17:43:45 -0400

    Changed the version of the game from JA to OpenJK in SP string.

--- code/qcommon/stv_version.h
+++ code/qcommon/stv_version.h
@@ -2,11 +2,11 @@
 #include "../win32/autoversion.h"
 
 #ifdef _DEBUG
-	#define	Q3_VERSION		"(debug)JA: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"(debug)OpenJK: v"VERSION_STRING_DOTTED
 #elif defined FINAL_BUILD
-	#define	Q3_VERSION		"JA: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"OpenJK: v"VERSION_STRING_DOTTED
 #else
-	#define	Q3_VERSION		"(internal)JA: v"VERSION_STRING_DOTTED
+	#define	Q3_VERSION		"(internal)OpenJK: v"VERSION_STRING_DOTTED
 #endif
 // end
 
