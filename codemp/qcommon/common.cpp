1f22aec3e3effc62f654c411780f1eb051f40be4
eezstreet <eezstreet@live.com>
2013-04-12 19:07:17 -0400

    Reverted 490abd9 AGAIN! >_<

--- codemp/qcommon/common.cpp
+++ codemp/qcommon/common.cpp
@@ -1677,7 +1677,7 @@ bool Com_ParseTextFile(const char *file, class CGenericParser2 &parser, bool cle
 
 	bufParse = buf;
 	parser.Parse(&bufParse, cleanFirst);
-	delete buf;
+	delete[] buf;
 
 	FS_FCloseFile( f );
 
@@ -1716,7 +1716,7 @@ CGenericParser2 *Com_ParseTextFile(const char *file, bool cleanFirst, bool write
 		parse = 0;
 	}
 
-	delete buf;
+	delete[] buf;
 
 	return parse;
 }
