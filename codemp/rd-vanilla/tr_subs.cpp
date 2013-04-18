bcee45093604787c97afa58793cce379952675b0
Ensiform <ensiform@gmail.com>
2013-04-17 20:22:25 -0500

    Fixed the oprintf commit with modular renderer...

--- codemp/rd-vanilla/tr_subs.cpp
+++ codemp/rd-vanilla/tr_subs.cpp
@@ -16,6 +16,18 @@ void QDECL Com_Printf( const char *msg, ... )
 	ri.Printf(PRINT_ALL, "%s", text);
 }
 
+void QDECL Com_OPrintf( const char *msg, ... )
+{
+	va_list         argptr;
+	char            text[1024];
+
+	va_start(argptr, msg);
+	Q_vsnprintf(text, sizeof(text), msg, argptr);
+	va_end(argptr);
+
+	ri.OPrintf("%s", text);
+}
+
 void QDECL Com_Error( int level, const char *error, ... )
 {
 	va_list         argptr;
