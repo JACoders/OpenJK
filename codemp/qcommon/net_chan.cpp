4710cceae0ea637512fb784d287af1b93922466d
TheSil <sildak@gmail.com>
2013-04-20 18:15:34 +0200

    Fix of negative ports

--- codemp/qcommon/net_chan.cpp
+++ codemp/qcommon/net_chan.cpp
@@ -403,12 +403,12 @@ const char	*NET_AdrToString (netadr_t a)
 	} else if (a.type == NA_BOT) {
 		Com_sprintf (s, sizeof(s), "bot");
 	} else if (a.type == NA_IP) {
-		Com_sprintf (s, sizeof(s), "%i.%i.%i.%i:%i",
+		Com_sprintf (s, sizeof(s), "%i.%i.%i.%i:%hu",
 			a.ip[0], a.ip[1], a.ip[2], a.ip[3], BigShort(a.port));
 	} else if (a.type == NA_BAD) {
 		Com_sprintf (s, sizeof(s), "BAD");
 	} else {
-		Com_sprintf (s, sizeof(s), "%02x%02x%02x%02x.%02x%02x%02x%02x%02x%02x:%i",
+		Com_sprintf (s, sizeof(s), "%02x%02x%02x%02x.%02x%02x%02x%02x%02x%02x:%hu",
 		a.ipx[0], a.ipx[1], a.ipx[2], a.ipx[3], a.ipx[4], a.ipx[5], a.ipx[6], a.ipx[7], a.ipx[8], a.ipx[9], 
 		BigShort(a.port));
 	}
