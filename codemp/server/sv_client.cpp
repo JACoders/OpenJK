3ba76783411d5aec4e49735c687aed12746f3573
Razish <mrrazish@gmail.com>
2013-04-24 23:00:02 +1000

    Add sv_filterCommands (default 0) so we can still use semicolons in chat.

--- codemp/server/sv_client.cpp
+++ codemp/server/sv_client.cpp
@@ -1398,7 +1398,9 @@ void SV_ExecuteClientCommand( client_t *cl, const char *s, qboolean clientOK ) {
 	if (clientOK) {
 		// pass unknown strings to the game
 		if (!u->name && sv.state == SS_GAME && (cl->state == CS_ACTIVE || cl->state == CS_PRIMED)) {
-			Cmd_Args_Sanitize();
+			// strip \r \n and ;
+			if ( sv_filterCommands->integer )
+				Cmd_Args_Sanitize();
 			VM_Call( gvm, GAME_CLIENT_COMMAND, cl - svs.clients );
 		}
 	}
