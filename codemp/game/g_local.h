fca5da7f401b6f5000d2e301d83a3635c4855f95
Ensiform <ensiform@gmail.com>
2013-05-05 18:29:30 -0500

    Use correct parameters for TeamCount function

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -1292,7 +1292,7 @@ qboolean CheckGauntletAttack( gentity_t *ent );
 //
 // g_client.c
 //
-team_t TeamCount( int ignoreClientNum, int team );
+int TeamCount( int ignoreClientNum, team_t team );
 int TeamLeader( int team );
 team_t PickTeam( int ignoreClientNum );
 void SetClientViewAngle( gentity_t *ent, vec3_t angle );
