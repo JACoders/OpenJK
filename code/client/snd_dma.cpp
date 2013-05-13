6110aef2d2de0c4cfb069a35b14f7f89d32d2404
Ensiform <ensiform@gmail.com>
2013-05-12 20:06:27 -0500

    Should fix offsetof not working in SP (snd_dma.cpp)

--- code/client/snd_dma.cpp
+++ code/client/snd_dma.cpp
@@ -335,6 +335,9 @@ const GUID EAX_REVERB_EFFECT = { 0xcf95c8f, 0xa3cc, 0x4849, { 0xb0, 0xb6, 0x83, 
 
 // instead of clearing a whole channel_t struct, we're going to skip the MP3SlidingDecodeBuffer[] buffer in the middle...
 //
+#ifndef offsetof
+#include <stddef.h>
+#endif
 static inline void Channel_Clear(channel_t *ch)
 {
 	// memset (ch, 0, sizeof(*ch));
