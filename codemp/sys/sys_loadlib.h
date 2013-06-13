c0db9869d6943af015a3af901dd6a0cb09333215
Ensiform <ensiform@gmail.com>
2013-06-13 13:08:25 -0500

    Remove old/wrong license header from sys_loadlib.h. Will be replaced in a coming commit with correct global license.

--- codemp/sys/sys_loadlib.h
+++ codemp/sys/sys_loadlib.h
@@ -1,35 +1,5 @@
 #pragma once
 
-/*
-===========================================================================
-Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company.
-
-This file is part of Spearmint Source Code.
-
-Spearmint Source Code is free software; you can redistribute it
-and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 3 of the License,
-or (at your option) any later version.
-
-Spearmint Source Code is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Spearmint Source Code.  If not, see <http://www.gnu.org/licenses/>.
-
-In addition, Spearmint Source Code is also subject to certain additional terms.
-You should have received a copy of these additional terms immediately following
-the terms and conditions of the GNU General Public License.  If not, please
-request a copy in writing from id Software at the address below.
-
-If you have questions concerning this license or the applicable additional
-terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc.,
-Suite 120, Rockville, Maryland 20850 USA.
-===========================================================================
-*/
-
 #ifdef DEDICATED
 #	ifdef _WIN32
 #		include <windows.h>
