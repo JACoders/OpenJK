0084da318ddb33f5913f71253eb9a1901070be94
Xycaleth <alex@acslo.com>
2013-06-24 23:41:56 +0100

    Started fixing aggressive warnings for codemp

--- codemp/renderer/tr_types.h
+++ codemp/renderer/tr_types.h
@@ -128,7 +128,9 @@ typedef struct miniRefEntity_s
 
 } miniRefEntity_t;
 
+#ifdef _MSC_VER
 #pragma warning (disable : 4201 )
+#endif
 typedef struct {
 	// this stucture must remain identical as the miniRefEntity_t
 	//
