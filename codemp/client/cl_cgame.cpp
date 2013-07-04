3d75a2bbbbc1ffb49cf13f8ed591553b6d3438b6
Ensiform <ensiform@gmail.com>
2013-07-04 01:19:13 -0500

    Remove unused variable resetTime from mp&sp client/cl_cgame.cpp

--- codemp/client/cl_cgame.cpp
+++ codemp/client/cl_cgame.cpp
@@ -1830,7 +1830,6 @@ or bursted delayed packets.
 #define	RESET_TIME	500
 
 void CL_AdjustTimeDelta( void ) {
-	int		resetTime;
 	int		newDelta;
 	int		deltaDelta;
 
@@ -1839,13 +1838,6 @@ void CL_AdjustTimeDelta( void ) {
 	// the delta never drifts when replaying a demo
 	if ( clc.demoplaying ) {
 		return;
-	}
-
-	// if the current time is WAY off, just correct to the current value
-	if ( com_sv_running->integer ) {
-		resetTime = 100;
-	} else {
-		resetTime = RESET_TIME;
 	}
 
 	newDelta = cl.snap.serverTime - cls.realtime;
