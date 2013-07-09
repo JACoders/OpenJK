2f80bb777a674a7887ddf65261aeb10e940f3a99
Ensiform <ensiform@gmail.com>
2013-07-09 17:44:39 -0500

    Merged idq3-GPL quotes fix for Com_ParseCommandLine to SP and MP.

--- code/qcommon/common.cpp
+++ code/qcommon/common.cpp
@@ -376,13 +376,17 @@ Break it up into multiple console lines
 ==================
 */
 void Com_ParseCommandLine( char *commandLine ) {
+	int inq = 0;
 	com_consoleLines[0] = commandLine;
 	com_numConsoleLines = 1;
 
 	while ( *commandLine ) {
+		if (*commandLine == '"') {
+			inq = !inq;
+		}
 		// look for a + seperating character
 		// if commandLine came from a file, we might have real line seperators
-		if ( *commandLine == '+' || *commandLine == '\n' ) {
+		if ( (*commandLine == '+' && !inq) || *commandLine == '\n'  || *commandLine == '\r' ) {
 			if ( com_numConsoleLines == MAX_CONSOLE_LINES ) {
 				return;
 			}
