0ee0e3ec7351d8fc34ab753ecdbe5473a242c074
Blaž Hrastnik <speed.the.bboy@gmail.com>
2013-07-11 13:36:05 +0200

    64bit pointer cast fix for debug builds

--- code/rd-vanilla/tr_init.cpp
+++ code/rd-vanilla/tr_init.cpp
@@ -1348,8 +1348,8 @@ void R_Init( void ) {
 //	Swap_Init();
 
 #ifndef FINAL_BUILD
-	if ( (int)tess.xyz & 15 ) {
-		Com_Printf( "WARNING: tess.xyz not 16 byte aligned (%x)\n",(int)tess.xyz & 15 );
+	if ( (intptr_t)tess.xyz & 15 ) {
+		Com_Printf( "WARNING: tess.xyz not 16 byte aligned (%x)\n",(intptr_t)tess.xyz & 15 );
 	}
 #endif
 
