39d6e83b5a73342d6f1eb0b09d5a91995fbcccce
Ensiform <ensiform@gmail.com>
2013-08-05 00:09:43 -0500

    Missed a reference to unused function.

--- codemp/sys/sys_local.h
+++ codemp/sys/sys_local.h
@@ -6,7 +6,6 @@ void 		IN_Shutdown( void );
 void 		IN_Restart( void );
 
 qboolean	Sys_GetPacket ( netadr_t *net_from, msg_t *net_message );
-void 		Sys_SendKeyEvents (void);
 char		*Sys_ConsoleInput (void);
 void 		Sys_QueEvent( int time, sysEventType_t type, int value, int value2, int ptrLength, void *ptr );
 void 		Sys_ListFilteredFiles( const char *basedir, char *subdirs, char *filter, char **psList, int *numfiles );
