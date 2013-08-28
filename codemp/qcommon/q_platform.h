b33c0f370b5356b5973e32ad9ac8963c8eca6920
Razish <mrrazish@gmail.com>
2013-08-29 06:45:28 +1000

    Hopefully fixed Linux build. Hopefully didn't break Mac build

--- codemp/qcommon/q_platform.h
+++ codemp/qcommon/q_platform.h
@@ -63,8 +63,6 @@
 // MAC OS X
 #elif defined(MACOS_X) || defined(__APPLE_CC__)
 
-	#include <unistd.h>
-
 	// make sure this is defined, just for sanity's sake...
 	#ifndef MACOS_X
 		#define MACOS_X
