964450f02e33bd5c3f6c17c97d62d1caeb3a13a0
Ensiform <ensiform@gmail.com>
2013-09-23 23:25:26 -0500

    Add cg_chatBeep and cg_teamChatBeep cvars to toggle the chat beep with global/tell and team chats.

--- codemp/cgame/cg_servercmds.c
+++ codemp/cgame/cg_servercmds.c
@@ -1613,7 +1613,8 @@ static void CG_ServerCommand( void ) {
 
 	if ( !strcmp( cmd, "chat" ) ) {
 		if ( !cg_teamChatsOnly.integer ) {
-			trap->S_StartLocalSound( cgs.media.talkSound, CHAN_LOCAL_SOUND );
+			if( cg_chatBeep.integer )
+				trap->S_StartLocalSound( cgs.media.talkSound, CHAN_LOCAL_SOUND );
 			Q_strncpyz( text, CG_Argv(1), MAX_SAY_TEXT );
 			CG_RemoveChatEscapeChar( text );
 			CG_ChatBox_AddString(text);
@@ -1623,7 +1624,8 @@ static void CG_ServerCommand( void ) {
 	}
 
 	if ( !strcmp( cmd, "tchat" ) ) {
-		trap->S_StartLocalSound( cgs.media.talkSound, CHAN_LOCAL_SOUND );
+		if( cg_teamChatBeep.integer )
+			trap->S_StartLocalSound( cgs.media.talkSound, CHAN_LOCAL_SOUND );
 		Q_strncpyz( text, CG_Argv(1), MAX_SAY_TEXT );
 		CG_RemoveChatEscapeChar( text );
 		CG_ChatBox_AddString(text);
@@ -1655,7 +1657,8 @@ static void CG_ServerCommand( void ) {
 				trap->SE_GetStringTextString(loc+1, loc, sizeof( loc ) );
 			}
 
-			trap->S_StartLocalSound( cgs.media.talkSound, CHAN_LOCAL_SOUND );
+			if( cg_chatBeep.integer )
+				trap->S_StartLocalSound( cgs.media.talkSound, CHAN_LOCAL_SOUND );
 			//Q_strncpyz( text, CG_Argv(1), MAX_SAY_TEXT );
 			Com_sprintf(text, sizeof( text ), "%s^7<%s> ^%s%s", name, loc, color, message);
 			CG_RemoveChatEscapeChar( text );
@@ -1685,7 +1688,8 @@ static void CG_ServerCommand( void ) {
 			trap->SE_GetStringTextString(loc+1, loc, sizeof( loc ) );
 		}
 
-		trap->S_StartLocalSound( cgs.media.talkSound, CHAN_LOCAL_SOUND );
+		if( cg_teamChatBeep.integer )
+			trap->S_StartLocalSound( cgs.media.talkSound, CHAN_LOCAL_SOUND );
 		//Q_strncpyz( text, CG_Argv(1), MAX_SAY_TEXT );
 		Com_sprintf(text, sizeof( text ), "%s^7<%s> ^%s%s", name, loc, color, message);
 		CG_RemoveChatEscapeChar( text );
