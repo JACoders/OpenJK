7249d5d6d1f961245ba79a0cedf316bef577a652
eezstreet <eezstreet@live.com>
2013-10-07 21:49:31 -0400

    Update rend2's tr_subs.cpp

--- codemp/rd-rend2/tr_subs.cpp
+++ codemp/rd-rend2/tr_subs.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include "tr_local.h"
 
+
 void QDECL Com_Printf( const char *msg, ... )
 {
 	va_list         argptr;
@@ -35,6 +36,18 @@ void QDECL Com_Printf( const char *msg, ... )
 	ri->Printf(PRINT_ALL, "%s", text);
 }
 
+void QDECL Com_OPrintf( const char *msg, ... )
+{
+	va_list         argptr;
+	char            text[1024];
+
+	va_start(argptr, msg);
+	Q_vsnprintf(text, sizeof(text), msg, argptr);
+	va_end(argptr);
+
+	ri->OPrintf("%s", text);
+}
+
 void QDECL Com_Error( int level, const char *error, ... )
 {
 	va_list         argptr;
@@ -45,4 +58,38 @@ void QDECL Com_Error( int level, const char *error, ... )
 	va_end(argptr);
 
 	ri->Error(level, "%s", text);
+}
+
+// HUNK
+void *Hunk_AllocateTempMemory( int size ) {
+	return ri->Hunk_AllocateTempMemory( size );
+}
+
+void Hunk_FreeTempMemory( void *buf ) {
+	ri->Hunk_FreeTempMemory( buf );
+}
+
+void *Hunk_Alloc( int size, ha_pref preference ) {
+	return ri->Hunk_Alloc( size, preference );
+}
+
+int Hunk_MemoryRemaining( void ) {
+	return ri->Hunk_MemoryRemaining();
+}
+
+// ZONE
+void *Z_Malloc( int iSize, memtag_t eTag, qboolean bZeroit, int iAlign ) {
+	return ri->Z_Malloc( iSize, eTag, bZeroit, iAlign );
+}
+
+void Z_Free( void *ptr ) {
+	ri->Z_Free( ptr );
+}
+
+int Z_MemSize( memtag_t eTag ) {
+	return ri->Z_MemSize( eTag );
+}
+
+void Z_MorphMallocTag( void *pvBuffer, memtag_t eDesiredTag ) {
+	ri->Z_MorphMallocTag( pvBuffer, eDesiredTag );
 }
