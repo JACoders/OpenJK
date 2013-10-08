168a9cf5fc35c33a8e4944c5a0f12dde9cdaaf55
eezstreet <eezstreet@live.com>
2013-10-07 20:52:19 -0400

    Add SDL stuff which is specific to rd-rend2

--- codemp/rd-rend2/qgl.h
+++ codemp/rd-rend2/qgl.h
@@ -26,11 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef __QGL_H__
 #define __QGL_H__
 
-#ifdef USE_LOCAL_HEADERS
-#	include "SDL_opengl.h"
-#else
-#	include <SDL_opengl.h>
-#endif
+#include "SDL/SDL_opengl.h"
 
 extern void (APIENTRYP qglActiveTextureARB) (GLenum texture);
 extern void (APIENTRYP qglClientActiveTextureARB) (GLenum texture);
