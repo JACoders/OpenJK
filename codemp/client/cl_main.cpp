0a6e03a0cf0572b47c67ac98319e2dc73a4cf38f
Ensiform <ensiform@gmail.com>
2013-10-15 00:05:34 -0500

    [MP] Merged critical fixes (ioquake/ioq3@f725b23) for snapshot floating point imprecision from ioq3 that were missing.
    Also merged: part of ioquake/ioq3@e41d081
    * Loopback clients only get snapshots at the server frame rate now
      (Anonymous <nkylqinhvgcbyl@mailinator.com>)
    * cl_lanForcePackets. When set to 0 (default is 1) the cl_maxpackets setting
      will be ignored if on a LAN. (Anonymous <nkylqinhvgcbyl@mailinator.com>)

--- codemp/client/cl_main.cpp
+++ codemp/client/cl_main.cpp
@@ -83,6 +83,8 @@ cvar_t	*cl_autolodscale;
 cvar_t	*cl_consoleKeys;
 #endif
 
+cvar_t  *cl_lanForcePackets;
+
 vec3_t cl_windVec;
 
 
@@ -2725,6 +2727,8 @@ void CL_Init( void ) {
 	cl_motdString = Cvar_Get( "cl_motdString", "", CVAR_ROM );
 
 	Cvar_Get( "cl_maxPing", "800", CVAR_ARCHIVE );
+
+	cl_lanForcePackets = Cvar_Get ("cl_lanForcePackets", "1", CVAR_ARCHIVE);
 
 	cl_guidServerUniq = Cvar_Get ("cl_guidServerUniq", "1", CVAR_ARCHIVE);
 
