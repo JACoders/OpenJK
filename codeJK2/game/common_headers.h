0ac79871929e5f5d8aa4dbf7656205a3666fc196
Nathaniel Brandes <ndbrandes@gmail.com>
2013-11-02 19:21:12 -0700

    Removed _NO_PCH and _IMMERSION references.
    _NO_PCH will always be true, and _IMMERSION references nothing, so out
    they go.

--- codeJK2/game/common_headers.h
+++ codeJK2/game/common_headers.h
@@ -32,13 +32,11 @@ This file is part of Jedi Knight 2.
 //	#include "../game/q_math.h"
 //#endif
 
-#ifdef _NO_PCH // (mrw) not sure how to make cmake use PCH, so they can be deactivated
 #define GAME_INCLUDE
 #include "../game/b_local.h"
 #include "../cgame/cg_local.h"
 #include "../game/g_navigator.h"
 #include "../game/g_shared.h"
 #include "../game/g_functions.h"
-#endif
 
 #endif // COMMON_HEADERS_H_INC
