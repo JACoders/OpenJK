2263b11299ba3a05b528b7dacf5e4a4ee02b2c8b
Nathaniel Brandes <ndbrandes@gmail.com>
2013-11-02 19:21:12 -0700

    Removed _NO_PCH and _IMMERSION references.
    _NO_PCH will always be true, and _IMMERSION references nothing, so out
    they go.

--- code/game/g_headers.h
+++ code/game/g_headers.h
@@ -28,8 +28,6 @@ This file is part of Jedi Academy.
 	#include "../game/teams.h"
 #endif
 
-// More stuff that we "need" on Xbox, as we don't use PCH
-#ifdef _NO_PCH // (mrw) not sure how to make cmake use PCH, so they can be deactivated
 #include "../game/b_local.h"
 #include "../game/g_functions.h"
 #include "../game/g_nav.h"
@@ -37,6 +35,5 @@ This file is part of Jedi Academy.
 #include "../game/g_vehicles.h"
 #include "../cgame/cg_camera.h"	// Just for AI_Rancor
 #include "../cgame/cg_local.h"	// Evil? Maybe. Necessary? Absolutely.
-#endif
 
 #endif // G_HEADERS_H_INC
