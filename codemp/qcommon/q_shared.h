e1dcd3a9d37c58a5f2c802e76a48baba7782e350
Ensiform <ensiform@gmail.com>
2013-11-09 22:00:22 -0600

    [MP] Make some of the networked arrays use the defines instead of hardcoded values. Useful for compatibility-breaking forked games.

--- codemp/qcommon/q_shared.h
+++ codemp/qcommon/q_shared.h
@@ -1509,7 +1509,9 @@ typedef enum {
 #define	MAX_STATS				16
 #define	MAX_PERSISTANT			16
 #define	MAX_POWERUPS			16
-#define	MAX_WEAPONS				19		
+#define	MAX_WEAPONS				19
+#define MAX_AMMO_TRANSMIT		16 // This is needed because the ammo array is 19 but only 16 sized array is networked
+#define MAX_AMMO				MAX_WEAPONS
 
 #define	MAX_PS_EVENTS			2
 
@@ -1617,7 +1619,7 @@ typedef struct playerState_s {
 	int			stats[MAX_STATS];
 	int			persistant[MAX_PERSISTANT];	// stats that aren't cleared on death
 	int			powerups[MAX_POWERUPS];	// level.time that the powerup runs out
-	int			ammo[MAX_WEAPONS];
+	int			ammo[MAX_AMMO];
 
 	int			generic1;
 	int			loopSound;
