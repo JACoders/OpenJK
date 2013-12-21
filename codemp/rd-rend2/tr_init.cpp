9568f2b1e3282ba18d9dfaa043de2e33693c17bf
Xycaleth <alex@acslo.com>
2013-12-21 18:45:56 +0000

    [Rend2] Fix few problems with lightmap code.
    This commit fixes a few things with the new light style code. It also
    disables lightmap merging for now as this was broken by the multiple
    lightmaps commit.

--- codemp/rd-rend2/tr_init.cpp
+++ codemp/rd-rend2/tr_init.cpp
@@ -1273,7 +1273,7 @@ void R_Register( void )
 	r_dlightMode = ri->Cvar_Get( "r_dlightMode", "0", CVAR_ARCHIVE | CVAR_LATCH );
 	r_pshadowDist = ri->Cvar_Get( "r_pshadowDist", "128", CVAR_ARCHIVE );
 	r_recalcMD3Normals = ri->Cvar_Get( "r_recalcMD3Normals", "0", CVAR_ARCHIVE | CVAR_LATCH );
-	r_mergeLightmaps = ri->Cvar_Get( "r_mergeLightmaps", "1", CVAR_ARCHIVE | CVAR_LATCH );
+	r_mergeLightmaps = ri->Cvar_Get( "r_mergeLightmaps", "0", CVAR_ARCHIVE | CVAR_LATCH );
 	r_imageUpsample = ri->Cvar_Get( "r_imageUpsample", "0", CVAR_ARCHIVE | CVAR_LATCH );
 	r_imageUpsampleMaxSize = ri->Cvar_Get( "r_imageUpsampleMaxSize", "1024", CVAR_ARCHIVE | CVAR_LATCH );
 	r_imageUpsampleType = ri->Cvar_Get( "r_imageUpsampleType", "1", CVAR_ARCHIVE | CVAR_LATCH );
