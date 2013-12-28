8b0449e342139de5f6e292f36b9042897aa8fc0f
Ensiform <ensiform@gmail.com>
2013-12-27 18:36:47 -0600

    [MP] Rend2: Merge fog fix from vanilla.

--- codemp/rd-rend2/tr_shader.cpp
+++ codemp/rd-rend2/tr_shader.cpp
@@ -2867,7 +2867,7 @@ static void FixRenderCommandList( int newShader ) {
 						sortedIndex = (( drawSurf->sort >> QSORT_SHADERNUM_SHIFT ) & (MAX_SHADERS-1));
 						if( sortedIndex >= newShader ) {
 							sortedIndex++;
-							drawSurf->sort = (sortedIndex << QSORT_SHADERNUM_SHIFT) | entityNum | ( fogNum << QSORT_FOGNUM_SHIFT ) | ( (int)postRender << QSORT_POSTRENDER_SHIFT) | (int)dlightMap;
+							drawSurf->sort = (sortedIndex << QSORT_SHADERNUM_SHIFT) | (entityNum << QSORT_REFENTITYNUM_SHIFT) | ( fogNum << QSORT_FOGNUM_SHIFT ) | ( (int)postRender << QSORT_POSTRENDER_SHIFT) | (int)dlightMap;
 						}
 					}
 					curCmd = (const void *)(ds_cmd + 1);
