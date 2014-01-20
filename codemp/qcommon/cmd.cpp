649941d04db2a0d5581825824a3b88a3912f3185
Ensiform <ensiform@gmail.com>
2014-01-20 15:53:09 -0600

    [MP] Merge cmd files into cmd.cpp. Improve cmdlist functionality. Add file buffer alias union.

--- codemp/qcommon/cmd.cpp
+++ codemp/qcommon/cmd.cpp
@@ -3,7 +3,6 @@
 //Anything above this #include will be ignored by the compiler
 #include "qcommon/exe_headers.h"
 
-
 #define	MAX_CMD_BUFFER	128*1024
 #define	MAX_CMD_LINE	1024
 
@@ -246,10 +245,7 @@ Cmd_Exec_f
 */
 void Cmd_Exec_f( void ) {
 	bool quiet;
-	union {
-		char	*c;
-		void	*v;
-	} f;
+	fileBuffer_t f;
 	char	filename[MAX_QPATH];
 
 	quiet = !Q_stricmp(Cmd_Argv(0), "execq");
@@ -317,11 +313,21 @@ void Cmd_Echo_f (void)
 =============================================================================
 */
 
+typedef struct cmd_function_s
+{
+	struct cmd_function_s	*next;
+	char					*name;
+	xcommand_t				function;
+	completionFunc_t		complete;
+} cmd_function_t;
+
 
 static	int			cmd_argc;
 static	char		*cmd_argv[MAX_STRING_TOKENS];		// points into cmd_tokenized
 static	char		cmd_tokenized[BIG_INFO_STRING+MAX_STRING_TOKENS];	// will have 0 bytes inserted
 static	char		cmd_cmd[BIG_INFO_STRING]; // the original command we received (no token processing)
+
+static	cmd_function_t	*cmd_functions;		// possible commands to execute
 
 
 /*
@@ -585,6 +591,205 @@ void Cmd_TokenizeStringIgnoreQuotes( const char *text_in ) {
 }
 
 /*
+============
+Cmd_FindCommand
+============
+*/
+cmd_function_t *Cmd_FindCommand( const char *cmd_name )
+{
+	cmd_function_t *cmd;
+	for( cmd = cmd_functions; cmd; cmd = cmd->next )
+		if( !Q_stricmp( cmd_name, cmd->name ) )
+			return cmd;
+	return NULL;
+}
+
+/*
+============
+Cmd_AddCommand
+============
+*/
+void	Cmd_AddCommand( const char *cmd_name, xcommand_t function ) {
+	cmd_function_t	*cmd;
+	
+	// fail if the command already exists
+	if( Cmd_FindCommand( cmd_name ) )
+	{
+		// allow completion-only commands to be silently doubled
+		if ( function != NULL ) {
+			Com_Printf ("Cmd_AddCommand: %s already defined\n", cmd_name);
+		}
+		return;
+	}
+
+	// use a small malloc to avoid zone fragmentation
+	cmd = (struct cmd_function_s *)S_Malloc (sizeof(cmd_function_t));
+	cmd->name = CopyString( cmd_name );
+	cmd->function = function;
+	cmd->complete = NULL;
+	cmd->next = cmd_functions;
+	cmd_functions = cmd;
+}
+
+/*
+============
+Cmd_SetCommandCompletionFunc
+============
+*/
+void Cmd_SetCommandCompletionFunc( const char *command, completionFunc_t complete ) {
+	for ( cmd_function_t *cmd=cmd_functions; cmd; cmd=cmd->next ) {
+		if ( !Q_stricmp( command, cmd->name ) )
+			cmd->complete = complete;
+	}
+}
+
+/*
+============
+Cmd_RemoveCommand
+============
+*/
+void	Cmd_RemoveCommand( const char *cmd_name ) {
+	cmd_function_t	*cmd, **back;
+
+	back = &cmd_functions;
+	while( 1 ) {
+		cmd = *back;
+		if ( !cmd ) {
+			// command wasn't active
+			return;
+		}
+		if ( !strcmp( cmd_name, cmd->name ) ) {
+			*back = cmd->next;
+			if (cmd->name) {
+				Z_Free(cmd->name);
+			}
+			Z_Free (cmd);
+			return;
+		}
+		back = &cmd->next;
+	}
+}
+
+/*
+============
+Cmd_CommandCompletion
+============
+*/
+void	Cmd_CommandCompletion( callbackFunc_t callback ) {
+	cmd_function_t	*cmd;
+	
+	for (cmd=cmd_functions ; cmd ; cmd=cmd->next) {
+		callback( cmd->name );
+	}
+}
+
+/*
+============
+Cmd_CompleteArgument
+============
+*/
+void Cmd_CompleteArgument( const char *command, char *args, int argNum ) {
+	for ( cmd_function_t *cmd=cmd_functions; cmd; cmd=cmd->next ) {
+		if ( !Q_stricmp( command, cmd->name ) && cmd->complete )
+			cmd->complete( args, argNum );
+	}
+}
+
+/*
+============
+Cmd_ExecuteString
+
+A complete command line has been parsed, so try to execute it
+============
+*/
+void	Cmd_ExecuteString( const char *text ) {	
+	cmd_function_t	*cmd, **prev;
+
+	// execute the command line
+	Cmd_TokenizeString( text );		
+	if ( !Cmd_Argc() ) {
+		return;		// no tokens
+	}
+
+	// check registered command functions	
+	for ( prev = &cmd_functions ; *prev ; prev = &cmd->next ) {
+		cmd = *prev;
+		if ( !Q_stricmp( Cmd_Argv(0), cmd->name ) ) {
+			// rearrange the links so that the command will be
+			// near the head of the list next time it is used
+			*prev = cmd->next;
+			cmd->next = cmd_functions;
+			cmd_functions = cmd;
+
+			// perform the action
+			if ( !cmd->function ) {
+				// let the cgame or game handle it
+				break;
+			} else {
+				cmd->function ();
+			}
+			return;
+		}
+	}
+	
+	// check cvars
+	if ( Cvar_Command() ) {
+		return;
+	}
+
+	// check client game commands
+	if ( com_cl_running && com_cl_running->integer && CL_GameCommand() ) {
+		return;
+	}
+
+	// check server game commands
+	if ( com_sv_running && com_sv_running->integer && SV_GameCommand() ) {
+		return;
+	}
+
+	// check ui commands
+	if ( com_cl_running && com_cl_running->integer && UI_GameCommand() ) {
+		return;
+	}
+
+	// send it as a server command if we are connected
+	// this will usually result in a chat message
+	//CL_ForwardCommandToServer ( text );
+	CL_ForwardCommandToServer ( text );
+}
+
+/*
+============
+Cmd_List_f
+============
+*/
+void Cmd_List_f (void)
+{
+	cmd_function_t	*cmd = NULL;
+	int				i, j;
+	char			*match = NULL;
+
+	if ( Cmd_Argc() > 1 ) {
+		match = Cmd_Argv( 1 );
+	}
+
+	for ( cmd=cmd_functions, i=0, j=0;
+		cmd;
+		cmd=cmd->next, i++ )
+	{
+		if ( !cmd->name || (match && !Com_Filter( match, cmd->name, qfalse )) )
+			continue;
+
+		Com_Printf (" %s\n", cmd->name);
+		j++;
+	}
+
+	Com_Printf ("\n%i total commands\n", i);
+	if ( i != j )
+		Com_Printf( "%i matching commands\n", j );
+}
+
+/*
 ==================
 Cmd_CompleteCfgName
 ==================
@@ -600,7 +805,6 @@ void Cmd_CompleteCfgName( char *args, int argNum ) {
 Cmd_Init
 ============
 */
-extern void Cmd_List_f(void);
 void Cmd_Init (void) {
 	Cmd_AddCommand ("cmdlist",Cmd_List_f);
 	Cmd_AddCommand ("exec",Cmd_Exec_f);
