65f8d909821aa7638d3078ba7f4a95393718dcce
Ensiform <ensiform@gmail.com>
2014-01-27 10:11:30 -0600

    [SP] JK2: Make sure ammoData_t::icon is of 64 size and not 32, matching a commit that was applied to JASP.

--- codeJK2/game/weapons.h
+++ codeJK2/game/weapons.h
@@ -158,11 +158,10 @@ typedef struct weaponData_s
 
 typedef struct ammoData_s
 {
-	char	icon[32];	// Name of ammo icon file
+	char	icon[64];	// Name of ammo icon file
 	int		max;		// Max amount player can hold of ammo
 } ammoData_t;
 
-// Moved from g_weapons.cpp. Seems like a much nicer place to have these here --eez
 // Bryar Pistol
 //--------
 #define BRYAR_PISTOL_VEL			1800
