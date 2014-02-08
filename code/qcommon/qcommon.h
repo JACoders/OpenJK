27321b82273207a1a51ef8462717e0a86657f05d
Xycaleth <alex@acslo.com>
2014-02-08 13:40:11 +0000

    [SP] Fix invalid G2 handles at vid_restart
    This commit is the counterpart to da6ddaaf34.

--- code/qcommon/qcommon.h
+++ code/qcommon/qcommon.h
@@ -853,4 +853,8 @@ inline int Round(float value)
 	return((int)floorf(value + 0.5f));
 }
 
+// Persistent data store API
+bool PD_Store ( const char *name, const void *data, size_t size );
+const void *PD_Load ( const char *name, size_t *size );
+
 #endif //__QCOMMON_H__
