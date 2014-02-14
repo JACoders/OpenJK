129e14506583e531087ba51563e91ed97e6115b0
Ensiform <ensiform@gmail.com>
2014-02-14 00:06:00 -0600

    [MP] Rend2: Use correct dlight image.

--- codemp/rd-rend2/tr_image.cpp
+++ codemp/rd-rend2/tr_image.cpp
@@ -2591,30 +2591,42 @@ R_CreateDlightImage
 */
 #define	DLIGHT_SIZE	16
 static void R_CreateDlightImage( void ) {
-	int		x,y;
-	byte	data[DLIGHT_SIZE][DLIGHT_SIZE][4];
-	int		b;
+	int		width, height;
+	byte	*pic;
 
-	// make a centered inverse-square falloff blob for dynamic lighting
-	for (x=0 ; x<DLIGHT_SIZE ; x++) {
-		for (y=0 ; y<DLIGHT_SIZE ; y++) {
-			float	d;
-
-			d = ( DLIGHT_SIZE/2 - 0.5f - x ) * ( DLIGHT_SIZE/2 - 0.5f - x ) +
-				( DLIGHT_SIZE/2 - 0.5f - y ) * ( DLIGHT_SIZE/2 - 0.5f - y );
-			b = 4000 / d;
-			if (b > 255) {
-				b = 255;
-			} else if ( b < 75 ) {
-				b = 0;
-			}
-			data[y][x][0] = 
-			data[y][x][1] = 
-			data[y][x][2] = b;
-			data[y][x][3] = 255;			
-		}
+	R_LoadImage("gfx/2d/dlight", &pic, &width, &height);
+	if (pic)
+	{
+		tr.dlightImage = R_CreateImage("*dlight", pic, width, height, IMGTYPE_COLORALPHA, IMGFLAG_CLAMPTOEDGE, 0 );
+		Z_Free(pic);
 	}
-	tr.dlightImage = R_CreateImage("*dlight", (byte *)data, DLIGHT_SIZE, DLIGHT_SIZE, IMGTYPE_COLORALPHA, IMGFLAG_CLAMPTOEDGE, 0 );
+	else
+	{	// if we dont get a successful load
+		int		x,y;
+		byte	data[DLIGHT_SIZE][DLIGHT_SIZE][4];
+		int		b;
+
+		// make a centered inverse-square falloff blob for dynamic lighting
+		for (x=0 ; x<DLIGHT_SIZE ; x++) {
+			for (y=0 ; y<DLIGHT_SIZE ; y++) {
+				float	d;
+
+				d = ( DLIGHT_SIZE/2 - 0.5f - x ) * ( DLIGHT_SIZE/2 - 0.5f - x ) +
+					( DLIGHT_SIZE/2 - 0.5f - y ) * ( DLIGHT_SIZE/2 - 0.5f - y );
+				b = 4000 / d;
+				if (b > 255) {
+					b = 255;
+				} else if ( b < 75 ) {
+					b = 0;
+				}
+				data[y][x][0] = 
+				data[y][x][1] = 
+				data[y][x][2] = b;
+				data[y][x][3] = 255;			
+			}
+		}
+		tr.dlightImage = R_CreateImage("*dlight", (byte *)data, DLIGHT_SIZE, DLIGHT_SIZE, IMGTYPE_COLORALPHA, IMGFLAG_CLAMPTOEDGE, 0 );
+	}
 }
 
 
