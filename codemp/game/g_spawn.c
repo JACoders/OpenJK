d8ea0051d316d0f29d253cbe704b52569dbdba4e
Ensiform <ensiform@gmail.com>
2014-03-03 11:58:37 -0600

    [MP] Ensure some data for world and none are filled out in the entity structure.

--- codemp/game/g_spawn.c
+++ codemp/game/g_spawn.c
@@ -1483,7 +1483,12 @@ void SP_worldspawn( void )
 	trap->SetConfigstring( CS_GLOBAL_AMBIENT_SET, text );
 
 	g_entities[ENTITYNUM_WORLD].s.number = ENTITYNUM_WORLD;
+	g_entities[ENTITYNUM_WORLD].r.ownerNum = ENTITYNUM_NONE;
 	g_entities[ENTITYNUM_WORLD].classname = "worldspawn";
+
+	g_entities[ENTITYNUM_NONE].s.number = ENTITYNUM_WORLD;
+	g_entities[ENTITYNUM_NONE].r.ownerNum = ENTITYNUM_NONE;
+	g_entities[ENTITYNUM_NONE].classname = "nothing";
 
 	// see if we want a warmup time
 	trap->SetConfigstring( CS_WARMUP, "" );
