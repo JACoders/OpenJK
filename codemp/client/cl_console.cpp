ddace74564bbf4ddcd3afec7cafc18b257b11859
Dion Williams <dionrhys1@gmail.com>
2014-04-01 15:43:17 +0100

    [MP] Add right margin to console version string. Very aesthetic. Wow.

--- codemp/client/cl_console.cpp
+++ codemp/client/cl_console.cpp
@@ -716,7 +716,7 @@ void Con_DrawSolidConsole( float frac ) {
 	i = strlen( JK_VERSION );
 
 	for (x=0 ; x<i ; x++) {
-		SCR_DrawSmallChar( cls.glconfig.vidWidth - ( i - x ) * SMALLCHAR_WIDTH,
+		SCR_DrawSmallChar( cls.glconfig.vidWidth - ( i - x + 1 ) * SMALLCHAR_WIDTH,
 			(lines-(SMALLCHAR_HEIGHT+SMALLCHAR_HEIGHT/2)), JK_VERSION[x] );
 	}
 
