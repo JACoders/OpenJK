d13b8a96094ac466aaa779fc51039f85377a2f6b
Razish <mrrazish@gmail.com>
2014-04-22 20:34:03 +1000

    [MP] Fix bad merge for processor affinity code on Windows

--- codemp/qcommon/common.cpp
+++ codemp/qcommon/common.cpp
@@ -1125,37 +1125,6 @@ static void Com_CatchError ( int code )
 	}
 }
 
-#ifdef _WIN32
-static const char *GetErrorString( DWORD error ) {
-	static char buf[MAX_STRING_CHARS];
-	buf[0] = '\0';
-
-	if ( error ) {
-		LPVOID lpMsgBuf;
-		DWORD bufLen = FormatMessage( FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-			NULL, error, MAKELANGID( LANG_NEUTRAL, SUBLANG_DEFAULT ), (LPTSTR)&lpMsgBuf, 0, NULL );
-		if ( bufLen ) {
-			LPCSTR lpMsgStr = (LPCSTR)lpMsgBuf;
-			Q_strncpyz( buf, lpMsgStr, min( (size_t)(lpMsgStr + bufLen), sizeof(buf) ) );
-			LocalFree( lpMsgBuf );
-		}
-	}
-	return buf;
-}
-#endif
-
-// based on Smod code
-static void Com_SetProcessorAffinity( void ) {
-#ifdef _WIN32
-	DWORD processMask;
-	if ( sscanf( com_affinity->string, "%X", &processMask ) != 1 )
-		processMask = 1; // set to first core only
-
-	if ( !SetProcessAffinityMask( GetCurrentProcess(), processMask ) )
-		Com_Printf( "Setting affinity mask failed (%s)\n", GetErrorString( GetLastError() ) );
-#endif
-}
-
 /*
 =================
 Com_Init
