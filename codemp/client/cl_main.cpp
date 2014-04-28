4d1364d7b02cb05ddd71524ab24e2de549b78e52
Ensiform <ensiform@gmail.com>
2014-04-28 14:41:10 -0500

    [MP] Remove teamtask cvar from cl_main

--- codemp/client/cl_main.cpp
+++ codemp/client/cl_main.cpp
@@ -2700,7 +2700,6 @@ void CL_Init( void ) {
 	Cvar_Get ("color1",  "4", CVAR_USERINFO | CVAR_ARCHIVE );
 	Cvar_Get ("color2", "4", CVAR_USERINFO | CVAR_ARCHIVE );
 	Cvar_Get ("handicap", "100", CVAR_USERINFO | CVAR_ARCHIVE );
-	Cvar_Get ("teamtask", "0", CVAR_USERINFO );
 	Cvar_Get ("sex", "male", CVAR_USERINFO | CVAR_ARCHIVE );
 	Cvar_Get ("password", "", CVAR_USERINFO);
 	Cvar_Get ("cg_predictItems", "1", CVAR_USERINFO | CVAR_ARCHIVE );
