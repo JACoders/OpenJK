90f14a41a1fd7810fd29e34c9165dcc77a493a82
Ensiform <ensiform@gmail.com>
2014-05-09 00:31:02 -0500

    [SP] Remove unused functions: R_GetShaderByNum, R_CreateExtendedName, R_CopyShader, R_CreateBlendedStage, R_MergeShaders, and R_CreateBlendedShader.

--- code/rd-vanilla/tr_bsp.cpp
+++ code/rd-vanilla/tr_bsp.cpp
@@ -304,19 +304,6 @@ static	void R_LoadVisibility( lump_t *l, world_t &worldData ) {
 
 //===============================================================================
 
-qhandle_t R_GetShaderByNum(int shaderNum, world_t &worldData)
-{
-	qhandle_t	shader;
-
-	if ( (shaderNum < 0) || (shaderNum >= worldData.numShaders) ) 
-	{
-		Com_Printf( "Warning: Bad index for R_GetShaderByNum - %i", shaderNum );
-		return(0);
-	}
-	shader = RE_RegisterShader(worldData.shaders[ shaderNum ].shader);
-	return(shader);
-}
-
 /*
 ===============
 ShaderForShaderNum
