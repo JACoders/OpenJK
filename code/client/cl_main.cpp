20b8df934e0412813690db991fd838e91b6d698d
Ensiform <ensiform@gmail.com>
2014-05-12 00:51:27 -0500

    [SP] Unused CL_NextDemo

--- code/client/cl_main.cpp
+++ code/client/cl_main.cpp
@@ -130,32 +130,6 @@ void CL_AddReliableCommand( const char *cmd ) {
 //======================================================================
 
 /*
-==================
-CL_NextDemo
-
-Called when a demo or cinematic finishes
-If the "nextdemo" cvar is set, that command will be issued
-==================
-*/
-void CL_NextDemo( void ) {
-	char	v[MAX_STRING_CHARS];
-
-	Q_strncpyz( v, Cvar_VariableString ("nextdemo"), sizeof(v) );
-	v[MAX_STRING_CHARS-1] = 0;
-	Com_DPrintf("CL_NextDemo: %s\n", v );
-	if (!v[0]) {
-		return;
-	}
-
-	Cvar_Set ("nextdemo","");
-	Cbuf_AddText (v);
-	Cbuf_AddText ("\n");
-	Cbuf_Execute();
-}
-
-//======================================================================
-
-/*
 =================
 CL_FlushMemory
 
