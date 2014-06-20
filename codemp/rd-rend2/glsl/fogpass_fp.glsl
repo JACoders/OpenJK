b5d4dd50ede450a760181bb9547dc22dd1661a83
Razish <mrrazish@gmail.com>
2014-06-20 10:14:21 +1000

    [MP] Rend2: Fix fog shader for dynamic glow. Patch by UniqueOne

--- codemp/rd-rend2/glsl/fogpass_fp.glsl
+++ codemp/rd-rend2/glsl/fogpass_fp.glsl
@@ -2,8 +2,16 @@ uniform vec4  u_Color;
 
 varying float var_Scale;
 
+out vec4 out_Glow;
+
 void main()
 {
 	gl_FragColor = u_Color;
 	gl_FragColor.a = sqrt(clamp(var_Scale, 0.0, 1.0));
+
+#if defined(USE_GLOW_BUFFER)
+	out_Glow = gl_FragColor;
+#else
+	out_Glow = vec4(0.0);
+#endif
 }
