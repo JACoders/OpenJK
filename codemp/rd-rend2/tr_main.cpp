305de7f69a781dc3c46ee3d7c7fbb3c9f7001b82
Ensiform <ensiform@gmail.com>
2014-07-25 10:15:39 -0500

    [MP] Rend2: Enable forcesight surfaces in rend2 as well.

--- codemp/rd-rend2/tr_main.cpp
+++ codemp/rd-rend2/tr_main.cpp
@@ -1688,7 +1688,7 @@ void R_AddDrawSurf( surfaceType_t *surface, shader_t *shader,
 		fogIndex = 0;
 	}
 
-	if ( (shader->surfaceFlags & SURF_FORCESIGHT) /*&& !(tr.refdef.rdflags & RDF_ForceSightOn)*/ )
+	if ( (shader->surfaceFlags & SURF_FORCESIGHT) && !(tr.refdef.rdflags & RDF_ForceSightOn) )
 	{	//if shader is only seen with ForceSight and we don't have ForceSight on, then don't draw
 		return;
 	}
