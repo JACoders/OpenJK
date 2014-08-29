8c42f181e5667c15fefb23545d5526a51690ad63
videoP <videoprofess@gmail.com>
2014-08-29 04:28:59 -0400

    fix

--- codemp/game/g_utils.c
+++ codemp/game/g_utils.c
@@ -821,7 +821,7 @@ gentity_t *G_Spawn( void ) {
 	if ( i == ENTITYNUM_MAX_NORMAL ) {
 		trap->SendServerCommand(-1, "print \"Warning: Entity limit reached!\n\"");
 		if (g_raceMode.integer == 2)
-			trap->SendConsoleCommand( EXEC_APPEND, "g_raceMode 1;map_restart 0;wait 20;g_raceMode 2\n" ); //kane, no.
+			trap->SendConsoleCommand( EXEC_APPEND, "g_raceMode 1;map_restart 0;g_raceMode 2\n" ); //kane, no.
 		else
 			trap->SendConsoleCommand( EXEC_APPEND, "map_restart 0\n" );
 		return e;
