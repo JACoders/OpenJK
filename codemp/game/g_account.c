541ceae52592ee36a7120d508b176b78346629a2
videoP <videoprofess@gmail.com>
2014-09-19 05:12:19 -0400

    eyy

--- codemp/game/g_account.c
+++ codemp/game/g_account.c
@@ -297,39 +297,36 @@ void G_AddDuel(char *winner, char *loser, int duration, int type, int winner_hp,
 void AddRunToWebServer(RaceRecord_t record) 
 { 
 #if 0
-    CURL *curl; 
-    CURLcode res; 
-    struct stat file_info; 
-    double speed_upload, total_time; 
-    FILE *fd; 
-	char address[128], data[256];
+	CURL *curl;
+	char address[128], data[256], password[64];
 
-	//Also send a password, just as another layer of security..?
 
 	Q_strncpyz(address, sv_webServerPath.string, sizeof(address));
+	Q_strncpyz(password, sv_webServerPassword.string, sizeof(password));
+
+	//Case, special chars matter? clean??  Encode coursename / username for html ?
+
 	Com_sprintf(data, sizeof(data), "username=%s&coursename=%s&duration_ms=%i&topspeed=%i&average=%i&style=%i&end_time=%i", 
 		record.username, record.coursename, record.duration_ms, record.topspeed, record.average, record.style, record.end_time);
 
-    curl = curl_easy_init(); 
+	curl_global_init(CURL_GLOBAL_ALL);
+	curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_VERBOSE, 1);
+	curl_easy_setopt(curl, CURLOPT_URL, address);
+	curl_easy_setopt(curl, CURLOPT_POST, 1);
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+	res = curl_easy_perform(curl); 
+	
+	if(res != CURLE_OK) 
+		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res)); 
+	else 
+		trap->Print("Received data:\n", g_buf); //de fuck izzat
 
-    if(curl) { 
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); 
-        curl_easy_setopt(curl,CURLOPT_CUSTOMREQUEST,"delete"); 
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION,&http_callback); 
-        curl_easy_setopt(curl, 
-		CURLOPT_URL, address); 
-        curl_easy_setopt(curl,CURLOPT_POSTFIELDS,"username=huancong&password=doyou"); 
-        res = curl_easy_perform(curl); 
-        if(res != CURLE_OK) { 
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", 
-                    curl_easy_strerror(res)); 
-        } 
-        else { 
-			trap->Print("Received data:\n", g_buf);
-        } 
-        curl_easy_cleanup(curl); 
-    } 
+	curl_easy_cleanup(curl);
 #endif
+
+
+
 } 
 
 void G_AddToDBFromFile(void) { //loda fixme, we can filter out the slower times from file before we add them??.. keep a record idk..?
