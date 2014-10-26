da23fae129f56741608f53745972cc13d03aebfd
videoP <videoprofess@gmail.com>
2014-10-26 17:04:35 -0400

    racemode wep drop, racemode falltodeath

--- codemp/game/g_cmds.c
+++ codemp/game/g_cmds.c
@@ -6555,6 +6555,7 @@ void Cmd_Race_f(gentity_t *ent)
 		ent->client->pers.raceMode = qfalse;
 		ent->client->pers.noFollow = qfalse;
 		ent->r.svFlags &= ~SVF_SINGLECLIENT; //ehh?
+		ent->s.weapon = WP_SABER; //Dont drop our weapon
 		trap->SendServerCommand(ent-g_entities, "print \"^5Race mode toggled off.\n\"");
 	}
 	else {
