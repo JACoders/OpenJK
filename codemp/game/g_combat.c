8dab2b5a89daf85f7510b507ebccf02d35e6b109
Zyk <zyk6870@gmail.com>
2014-12-15 02:43:44 -0200

    Lowered bonus damage of some bosses.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_combat.c
+++ codemp/game/g_combat.c
@@ -4938,7 +4938,7 @@ void G_Damage( gentity_t *targ, gentity_t *inflictor, gentity_t *attacker, vec3_
 	{
 		if (attacker->client->pers.guardian_mode == 12 || attacker->client->pers.guardian_mode == 13 || attacker->client->pers.guardian_mode == 15 || attacker->client->pers.universe_quest_artifact_holder_id != -1)
 		{ // zyk: these quest npcs have bonus damage
-			damage = (int)ceil(damage*1.2);
+			damage = (int)ceil(damage*1.15);
 		}
 	}
 
