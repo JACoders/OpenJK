b52904ed0f848f633449fa0c85360664b6c06c41
Simon McVittie <smcv@debian.org>
2014-12-25 23:32:40 +0000

    [SP, MP] Replace GPL-3+ sys/sys_loadlib.h with GPL-2+ version from ioquake3
    The one in OpenJK is functionally equivalent, but has GPL-3+ headers
    because it came via one of the Return to Castle Wolfenstein family
    of games. This is the only file I found in OpenJK that is not GPL-2
    compatible, so it seems sensible to simplify the licensing.

--- codemp/sys/sys_loadlib.h
+++ codemp/sys/sys_loadlib.h
@@ -1,4 +1,24 @@
-#pragma once
+/*
+===========================================================================
+Copyright (C) 1999-2005 Id Software, Inc.
+
+This file is part of Quake III Arena source code.
+
+Quake III Arena source code is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+Quake III Arena source code is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Quake III Arena source code; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
+*/
 
 #ifdef DEDICATED
 #	ifdef _WIN32
@@ -15,8 +35,13 @@
 #		define Sys_LibraryError() dlerror()
 #	endif
 #else
-#	include <SDL.h>
-#	include <SDL_loadso.h>
+#	ifdef USE_LOCAL_HEADERS
+#		include "SDL.h"
+#		include "SDL_loadso.h"
+#	else
+#		include <SDL.h>
+#		include <SDL_loadso.h>
+#	endif
 #	define Sys_LoadLibrary(f) SDL_LoadObject(f)
 #	define Sys_UnloadLibrary(h) SDL_UnloadObject(h)
 #	define Sys_LoadFunction(h,fn) SDL_LoadFunction(h,fn)
