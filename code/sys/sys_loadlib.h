180d48da7f552a4124b80ecf23340bf9f3b075f2
Ensiform <ensiform@gmail.com>
2015-01-02 15:56:46 -0600

    [SP] Nuking the license header all together in code/sys/sys_loadlib.h since it is the same as the MP one which doesn't have the explicit header.  Implied still GPLv2.

--- code/sys/sys_loadlib.h
+++ code/sys/sys_loadlib.h
@@ -1,35 +1,4 @@
-/*
-===========================================================================
-Copyright (C) 1999-2010 id Software LLC, a ZeniMax Media company.
-
-This file is part of Spearmint Source Code.
-
-Spearmint Source Code is free software; you can redistribute it
-and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 3 of the License,
-or (at your option) any later version.
-
-Spearmint Source Code is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Spearmint Source Code.  If not, see <http://www.gnu.org/licenses/>.
-
-In addition, Spearmint Source Code is also subject to certain additional terms.
-You should have received a copy of these additional terms immediately following
-the terms and conditions of the GNU General Public License.  If not, please
-request a copy in writing from id Software at the address below.
-
-If you have questions concerning this license or the applicable additional
-terms, you may contact in writing id Software LLC, c/o ZeniMax Media Inc.,
-Suite 120, Rockville, Maryland 20850 USA.
-===========================================================================
-*/
-
-#ifndef SYS_LOADLIB_H_
-#define SYS_LOADLIB_H_
+#pragma once
 
 #ifdef DEDICATED
 #	ifdef _WIN32
@@ -55,5 +24,3 @@ Suite 120, Rockville, Maryland 20850 USA.
 #endif
 
 void * QDECL Sys_LoadDll(const char *name, qboolean useSystemLib);
-
-#endif /* SYS_LOADLIB_H_ */
