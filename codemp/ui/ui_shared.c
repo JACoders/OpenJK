b2f71a9c485a039d5edad22fa617769a1a7231e8
Ensiform <ensiform@gmail.com>
2015-01-14 16:48:40 -0600

    [MP] Fix extra newlines being appended to PC_SourceError calls.

--- codemp/ui/ui_shared.c
+++ codemp/ui/ui_shared.c
@@ -385,7 +385,7 @@ qboolean PC_Float_Parse(int handle, float *f) {
 		negative = qtrue;
 	}
 	if (token.type != TT_NUMBER) {
-		PC_SourceError(handle, "expected float but found %s\n", token.string);
+		PC_SourceError(handle, "expected float but found %s", token.string);
 		return qfalse;
 	}
 	if (negative)
@@ -465,7 +465,7 @@ qboolean PC_Int_Parse(int handle, int *i) {
 		negative = qtrue;
 	}
 	if (token.type != TT_NUMBER) {
-		PC_SourceError(handle, "expected integer but found %s\n", token.string);
+		PC_SourceError(handle, "expected integer but found %s", token.string);
 		return qfalse;
 	}
 	*i = token.intvalue;
@@ -8091,7 +8091,7 @@ qboolean ItemParse_cvarStrList( itemDef_t *item, int handle ) {
 
 		if (!PC_String_Parse(handle, (const char **)&psString))
 		{
-			PC_SourceError(handle, "end of file inside menu item\n");
+			PC_SourceError(handle, "end of file inside menu item");
 			return qfalse;
 		}
 
@@ -8153,7 +8153,7 @@ qboolean ItemParse_cvarFloatList( itemDef_t *item, int handle )
 
 		if ( !PC_String_Parse ( handle, (const char **)&string ) )
 		{
-			PC_SourceError(handle, "end of file inside menu item\n");
+			PC_SourceError(handle, "end of file inside menu item");
 			return qfalse;
 		}
 
@@ -8442,7 +8442,7 @@ qboolean Item_Parse(int handle, itemDef_t *item) {
 	}
 	while ( 1 ) {
 		if (!trap->PC_ReadToken(handle, &token)) {
-			PC_SourceError(handle, "end of file inside menu item\n");
+			PC_SourceError(handle, "end of file inside menu item");
 			return qfalse;
 		}
 
@@ -9208,7 +9208,7 @@ qboolean Menu_Parse(int handle, menuDef_t *menu) {
 
 	while ( 1 ) {
 		if (!trap->PC_ReadToken(handle, &token)) {
-			PC_SourceError(handle, "end of file inside menu\n");
+			PC_SourceError(handle, "end of file inside menu");
 			return qfalse;
 		}
 
