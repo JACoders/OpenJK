2ea1bbbfc259a84ffd0ab74d7613183223f61587
Ensiform <ensiform@gmail.com>
2015-01-26 09:50:04 -0600

    [MP] Fixed overlapping `s_channels` variable name in S_RawSamples.

--- codemp/client/snd_dma.cpp
+++ codemp/client/snd_dma.cpp
@@ -2116,7 +2116,7 @@ S_RawSamples
 Music streaming
 ============
 */
-void S_RawSamples( int samples, int rate, int width, int s_channels, const byte *data, float volume, int bFirstOrOnlyUpdateThisFrame )
+void S_RawSamples( int samples, int rate, int width, int channels, const byte *data, float volume, int bFirstOrOnlyUpdateThisFrame )
 {
 	int		i;
 	int		src, dst;
@@ -2137,7 +2137,7 @@ void S_RawSamples( int samples, int rate, int width, int s_channels, const byte 
 	scale = (float)rate / dma.speed;
 
 //Com_Printf ("%i < %i < %i\n", s_soundtime, s_paintedtime, s_rawend);
-	if (s_channels == 2 && width == 2)
+	if (channels == 2 && width == 2)
 	{
 		if (scale == 1.0)
 		{	// optimized case
@@ -2192,7 +2192,7 @@ void S_RawSamples( int samples, int rate, int width, int s_channels, const byte 
 			}
 		}
 	}
-	else if (s_channels == 1 && width == 2)
+	else if (channels == 1 && width == 2)
 	{
 		if (bFirstOrOnlyUpdateThisFrame)
 		{
@@ -2221,7 +2221,7 @@ void S_RawSamples( int samples, int rate, int width, int s_channels, const byte 
 			}
 		}
 	}
-	else if (s_channels == 2 && width == 1)
+	else if (channels == 2 && width == 1)
 	{
 		intVolume *= 256;
 
@@ -2252,7 +2252,7 @@ void S_RawSamples( int samples, int rate, int width, int s_channels, const byte 
 			}
 		}
 	}
-	else if (s_channels == 1 && width == 1)
+	else if (channels == 1 && width == 1)
 	{
 		intVolume *= 256;
 
