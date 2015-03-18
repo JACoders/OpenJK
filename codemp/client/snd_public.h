02eb3e92f2ba21b89bd597eb8580f1b57bed7888
Simon McVittie <smcv@debian.org>
2015-03-18 09:22:43 +0000

    Change Raven and Activision copyright to include older years
    Some of the older copyright boilerplate removed by 59dfa47 said
    things like
    
    // Copyright 2001-2013 Raven Software
    // Copyright 2000-2001 Raven Software
    // Copyright 2001-2013 Activision
    
    so we should preserve that. It seems safest to assume that this
    applies to to everything.
    
    Change automated by:
    
    find * -type f -print0 | xargs -0 perl -p -i -e \
    's/(Copyright \(C\) )2002( - 2013, Raven Software, Inc\.)/$1."2000".$2/eg'
    find * -type f -print0 | xargs -0 perl -p -i -e \
    's/(Copyright \(C\) )2002( - 2013, Activision, Inc\.)/$1."2001".$2/eg'

--- codemp/client/snd_public.h
+++ codemp/client/snd_public.h
@@ -1,8 +1,8 @@
 /*
 ===========================================================================
 Copyright (C) 1999 - 2005, Id Software, Inc.
-Copyright (C) 2002 - 2013, Raven Software, Inc.
-Copyright (C) 2002 - 2013, Activision, Inc.
+Copyright (C) 2000 - 2013, Raven Software, Inc.
+Copyright (C) 2001 - 2013, Activision, Inc.
 Copyright (C) 2013 - 2015, OpenJK contributors
 
 This file is part of the OpenJK source code.
