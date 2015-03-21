c0281710ce9881a5d2918df7b8ea28cd2e4d8149
Simon McVittie <smcv@debian.org>
2015-03-21 15:21:28 +0000

    InstallZIP.cmake: add "OpenJK contributors" as a copyright holder
    This currently means @xycaleth.

--- CMakeModules/InstallZIP.cmake
+++ CMakeModules/InstallZIP.cmake
@@ -1,5 +1,6 @@
 #=============================================================================
 # Copyright 2007-2009 Kitware, Inc.
+# Copyright 2015 OpenJK contributors
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
