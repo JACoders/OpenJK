f7558bf78b43198ea07821cfafa2b8b842ff2212
Zyk <zyk6870@gmail.com>
2015-05-23 15:08:33 -0300

    Force Tank Unique Skill lasts less but has more resistance to damage.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_combat.c
+++ codemp/game/g_combat.c
@@ -5048,8 +5048,8 @@ void G_Damage( gentity_t *targ, gentity_t *inflictor, gentity_t *attacker, vec3_
 			float unique_skill_resistance = 0.0;
 
 			if (targ->client->ps.powerups[PW_NEUTRALFLAG] > level.time)
-			{
-				unique_skill_resistance += 0.05;
+			{ // zyk: Unique Skill increases resistance to damage
+				unique_skill_resistance += 0.1;
 			}
 
 			if (targ->client->pers.secrets_found & (1 << 19))
