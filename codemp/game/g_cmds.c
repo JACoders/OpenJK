594486937c68146668849aceceb19b9c35939c04
Zyk <zyk6870@gmail.com>
2015-06-21 01:46:21 -0300

    Reduced cost of some items in jawa seller.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_cmds.c
+++ codemp/game/g_cmds.c
@@ -9429,7 +9429,7 @@ void Cmd_Stuff_f( gentity_t *ent ) {
 		}
 		else if (Q_stricmp(arg1, "items" ) == 0)
 		{
-			trap->SendServerCommand( ent-g_entities, "print \"\n^39 - Shield Booster: ^7Buy: 250 - Sell: ^1no\n^310 - Sentry Gun: ^7Buy: 200 - Sell: 60\n^311 - Seeker Drone: ^7Buy: 230 - Sell: 65\n^312 - Big Bacta: ^7Buy: 300 - Sell: 70\n^313 - Force Field: ^7Buy: 400 - Sell: 80\n^314 - Ysalamiri: ^7Buy: 200 - Sell: 50\n^331 - Jetpack Fuel: ^7Buy: 300 - Sell: ^1no\n^334 - Bacta Canister: ^7Buy: 100 - Sell: 20\n^335 - E-Web: ^7Buy: 150 - Sell: 30\n^338 - Binoculars: ^7Buy: 10 - Sell: 5\n^341 - Jetpack: ^7Buy: 50 - Sell: ^1no\n^342 - Cloak Item: ^7Buy: 50 - Sell: 20\n^343 - Force Boon: ^7Buy: 200 - Sell: 50\n^344 - Magic Potion: ^7Buy: 50 - Sell: ^1no\n^348 - Force Armor: ^7Buy: 3000 - Sell: ^1no\n^349 - Saber Armor: ^7Buy: 3000 - Sell: ^1no\n^350 - Gun Armor: ^7Buy: 3000 - Sell: ^1no\n^351 - Healing Crystal: ^7Buy: 3000 - Sell: ^1no\n^352 - Energy Crystal: ^7Buy: 3000 - Sell: ^1no^7\n\n\"");
+			trap->SendServerCommand( ent-g_entities, "print \"\n^39 - Shield Booster: ^7Buy: 250 - Sell: ^1no\n^310 - Sentry Gun: ^7Buy: 190 - Sell: 60\n^311 - Seeker Drone: ^7Buy: 200 - Sell: 65\n^312 - Big Bacta: ^7Buy: 250 - Sell: 70\n^313 - Force Field: ^7Buy: 350 - Sell: 80\n^314 - Ysalamiri: ^7Buy: 200 - Sell: 50\n^331 - Jetpack Fuel: ^7Buy: 300 - Sell: ^1no\n^334 - Bacta Canister: ^7Buy: 100 - Sell: 20\n^335 - E-Web: ^7Buy: 150 - Sell: 30\n^338 - Binoculars: ^7Buy: 10 - Sell: 5\n^341 - Jetpack: ^7Buy: 50 - Sell: ^1no\n^342 - Cloak Item: ^7Buy: 50 - Sell: 20\n^343 - Force Boon: ^7Buy: 200 - Sell: 50\n^344 - Magic Potion: ^7Buy: 50 - Sell: ^1no\n^348 - Force Armor: ^7Buy: 3000 - Sell: ^1no\n^349 - Saber Armor: ^7Buy: 3000 - Sell: ^1no\n^350 - Gun Armor: ^7Buy: 3000 - Sell: ^1no\n^351 - Healing Crystal: ^7Buy: 3000 - Sell: ^1no\n^352 - Energy Crystal: ^7Buy: 3000 - Sell: ^1no^7\n\n\"");
 		}
 		else if (Q_stricmp(arg1, "weapons" ) == 0)
 		{
@@ -9658,7 +9658,7 @@ Cmd_Buy_f
 void Cmd_Buy_f( gentity_t *ent ) {
 	char arg1[MAX_STRING_CHARS];
 	int value = 0;
-	int item_costs[NUMBER_OF_SELLER_ITEMS] = {20,25,30,50,100,140,170,5000,250,200,230,300,400,200,5000,3000,100,120,150,200,110,90,170,300,2000,1500,2500,3000,5000,200,300,20,1000,100,150,150,90,10,5000,3000,50,50,200,50,5000,10000,5000,3000,3000,3000,3000,3000};
+	int item_costs[NUMBER_OF_SELLER_ITEMS] = {20,25,30,50,100,140,170,5000,250,190,200,250,350,200,5000,3000,100,120,150,200,110,90,170,300,2000,1500,2500,3000,5000,200,300,20,1000,100,150,150,90,10,5000,3000,50,50,200,50,5000,10000,5000,3000,3000,3000,3000,3000};
 
 	if (trap->Argc() == 1)
 	{
