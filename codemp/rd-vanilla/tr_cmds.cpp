JACoders/OpenJK

commit 27c166ff27cfdaf426f821b20eefdaa2f65b148c

Cleanup unused variables under non-Debug builds after MinGW changes.

codemp/rd-vanilla/tr_cmds.cpp
@@ -390,11 +390,9 @@ void RE_BeginFrame( stereoFrame_t stereoFrame ) {
 
     // check for errors
     if ( !r_ignoreGLErrors->integer ) {
-        int	err;
-
 		R_IssuePendingRenderCommands();
 #if defined(_DEBUG)
-        if ( ( err = qglGetError() ) != GL_NO_ERROR ) {
+		if ( ( GLenum err = qglGetError() ) != GL_NO_ERROR ) {
             Com_Error( ERR_FATAL, "RE_BeginFrame() - glGetError() failed (0x%x)!\n", err );
         }
 #endif

codemp/rd-vanilla/tr_init.cpp
@@ -829,10 +829,10 @@ GL_CheckErrors
 ==================
 */
 void GL_CheckErrors( void ) {
-    int		err;
+#if defined(_DEBUG)
+    GLenum	err;
     char	s[64];
 
-#if defined(_DEBUG)
     err = qglGetError();
     if ( err == GL_NO_ERROR ) {
         return;

commit 90596e4e0abf7d3b3e7934f645e6aad5897f24ed

MP: Fix build error introduced in 27c166f

codemp/rd-vanilla/tr_cmds.cpp
@@ -392,7 +392,8 @@ void RE_BeginFrame( stereoFrame_t stereoFrame ) {
     if ( !r_ignoreGLErrors->integer ) {
 		R_IssuePendingRenderCommands();
 #if defined(_DEBUG)
-		if ( ( GLenum err = qglGetError() ) != GL_NO_ERROR ) {
+		 GLenum err = qglGetError();
+		if ( err != GL_NO_ERROR ) {
             Com_Error( ERR_FATAL, "RE_BeginFrame() - glGetError() failed (0x%x)!\n", err );
         }
 #endif