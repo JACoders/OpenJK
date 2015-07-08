2cfd236c5e4414ee489e556247cdbb9cbdd466ea
dusty22 <creinhardt228@yahoo.com>
2015-07-08 15:35:15 -0400

    Game version is "Dusty's Patch" instead of "OpenJK"

--- code/game/g_local.h
+++ code/game/g_local.h
@@ -37,7 +37,7 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"OpenJK"
+#define	GAMEVERSION	"Dusty's Patch"
 
 #define BODY_QUEUE_SIZE		8
 
