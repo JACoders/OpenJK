66cdc1207f590747cc6bd263acbbb7705f51c462
Ensiform <ensiform@gmail.com>
2015-07-09 15:21:55 -0500

    [SP] Fix #673 by removing the inline from the functions. This is not compatible with function pointers on newer GCC versions.

--- code/game/g_vehicles.cpp
+++ code/game/g_vehicles.cpp
@@ -56,7 +56,6 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #ifndef _JK2MP
-#define GAME_INLINE inline
 #define bgEntity_t gentity_t
 #endif
 
@@ -3153,16 +3152,16 @@ void G_VehUpdateShields( gentity_t *targ )
 #endif
 
 // Set the parent entity of this Vehicle NPC.
-GAME_INLINE void SetParent( Vehicle_t *pVeh, bgEntity_t *pParentEntity ) { pVeh->m_pParentEntity = pParentEntity; }
+void SetParent( Vehicle_t *pVeh, bgEntity_t *pParentEntity ) { pVeh->m_pParentEntity = pParentEntity; }
 
 // Add a pilot to the vehicle.
-GAME_INLINE void SetPilot( Vehicle_t *pVeh, bgEntity_t *pPilot ) { pVeh->m_pPilot = pPilot; }
+void SetPilot( Vehicle_t *pVeh, bgEntity_t *pPilot ) { pVeh->m_pPilot = pPilot; }
 
 // Add a passenger to the vehicle (false if we're full).
-GAME_INLINE bool AddPassenger( Vehicle_t *pVeh ) { return false; }
+bool AddPassenger( Vehicle_t *pVeh ) { return false; }
 
 // Whether this vehicle is currently inhabited (by anyone) or not.
-GAME_INLINE bool Inhabited( Vehicle_t *pVeh ) { return ( pVeh->m_pPilot ) ? true : false; }
+bool Inhabited( Vehicle_t *pVeh ) { return ( pVeh->m_pPilot ) ? true : false; }
 
 
 // Setup the shared functions (one's that all vehicles would generally use).
