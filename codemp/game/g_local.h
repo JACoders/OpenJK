bf89318a8641cc3745f7699435c6a64d6d6eee21
Zyk <zyk6870@gmail.com>
2015-07-20 22:12:37 -0300

    Changed GAMEVERSION to 3.15.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -22,7 +22,7 @@ extern vec3_t gPainPoint;
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"Zyk OpenJK Mod v3.14"
+#define	GAMEVERSION	"Zyk OpenJK Mod v3.15"
 
 #define SECURITY_LOG "security.log"
 
