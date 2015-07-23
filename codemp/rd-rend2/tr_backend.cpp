JACoders/OpenJK

commit 8325f6cf11204401de0db4fbe6906d156efe6d0f

Fixed transparency and updated BSP code for RBSP
Fixed objects which were transparent and being rendered even when
occluded. Also updated some of the BSP code to use RBSP format properly.
This includes things such as the lighting grid array.

codemp/rd-rend2/tr_backend.cpp
@@ -563,7 +563,7 @@ void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 	int				dlighted, oldDlighted;
 	int				pshadowed, oldPshadowed;
 	int             cubemapIndex, oldCubemapIndex;
-	qboolean		depthRange, oldDepthRange;
+	int		depthRange, oldDepthRange;
 	int				i;
 	drawSurf_t		*drawSurf;
 	int				oldSort;
@@ -584,7 +584,7 @@ void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 	backEnd.currentEntity = &tr.worldEntity;
 	oldShader = NULL;
 	oldFogNum = -1;
-	oldDepthRange = qfalse;
+	oldDepthRange = 0;
 	oldDlighted = qfalse;
 	oldPshadowed = qfalse;
 	oldCubemapIndex = -1;
@@ -634,7 +634,7 @@ void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 		//
 		if ( entityNum != oldEntityNum ) {
 			qboolean sunflare = qfalse;
-			depthRange = qfalse;
+			depthRange = 0;
 
 			if ( entityNum != REFENTITYNUM_WORLD ) {
 				backEnd.currentEntity = &backEnd.refdef.entities[entityNum];
@@ -651,10 +651,13 @@ void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 					R_TransformDlights( backEnd.refdef.num_dlights, backEnd.refdef.dlights, &backEnd.or );
 				}
 
-				if(backEnd.currentEntity->e.renderfx & RF_DEPTHHACK)
-				{
+				if ( backEnd.currentEntity->e.renderfx & RF_NODEPTH ) {
+					// No depth at all, very rare but some things for seeing through walls
+					depthRange = 2;
+				}
+				else if ( backEnd.currentEntity->e.renderfx & RF_DEPTHHACK ) {
 					// hack the depth range to prevent view model from poking into walls
-					depthRange = qtrue;
+					depthRange = 1;
 				}
 			} else {
 				backEnd.currentEntity = &tr.worldEntity;
@@ -674,36 +677,56 @@ void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 			//
 			if (oldDepthRange != depthRange)
 			{
-				if (depthRange)
-				{
-					if(backEnd.viewParms.stereoFrame != STEREO_CENTER)
-					{
-						viewParms_t temp = backEnd.viewParms;
+				switch ( depthRange ) {
+					default:
+					case 0:
+						if(backEnd.viewParms.stereoFrame != STEREO_CENTER)
+						{
+							GL_SetProjectionMatrix( backEnd.viewParms.projectionMatrix );
+						}
 
-						R_SetupProjection(&temp, r_znear->value, 0, qfalse);
+						if (!sunflare)
+							qglDepthRange (0, 1);
 
-						GL_SetProjectionMatrix( temp.projectionMatrix );
-					}
-
- 					if(!oldDepthRange)
-					{
 						depth[0] = 0;
-						depth[1] = 0.3f;
- 						qglDepthRange (depth[0], depth[1]);
-	 				}
-				}
-				else
-				{
-					if(backEnd.viewParms.stereoFrame != STEREO_CENTER)
-					{
-						GL_SetProjectionMatrix( backEnd.viewParms.projectionMatrix );
-					}
+						depth[1] = 1;
+						break;
 
-					if (!sunflare)
-						qglDepthRange (0, 1);
+					case 1:
+						if(backEnd.viewParms.stereoFrame != STEREO_CENTER)
+						{
+							viewParms_t temp = backEnd.viewParms;
 
-					depth[0] = 0;
-					depth[1] = 1;
+							R_SetupProjection(&temp, r_znear->value, 0, qfalse);
+
+							GL_SetProjectionMatrix( temp.projectionMatrix );
+						}
+
+ 						if(!oldDepthRange)
+						{
+							depth[0] = 0;
+							depth[1] = 0.3f;
+ 							qglDepthRange (depth[0], depth[1]);
+	 					}
+						break;
+
+					case 2:
+						if(backEnd.viewParms.stereoFrame != STEREO_CENTER)
+						{
+							viewParms_t temp = backEnd.viewParms;
+
+							R_SetupProjection(&temp, r_znear->value, 0, qfalse);
+
+							GL_SetProjectionMatrix( temp.projectionMatrix );
+						}
+
+ 						if(!oldDepthRange)
+						{
+							depth[0] = 0.0f;
+							depth[1] = 0.0f;
+ 							qglDepthRange (depth[0], depth[1]);
+	 					}
+						break;
 				}
 
 				oldDepthRange = depthRange;
@@ -734,7 +757,7 @@ void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 
 	GL_SetModelviewMatrix( backEnd.viewParms.world.modelMatrix );
 
-	qglDepthRange (0, 1);
+	//qglDepthRange (0, 1);
 }
 
 

codemp/rd-rend2/tr_bsp.cpp
@@ -2641,7 +2641,6 @@ R_LoadLightGrid
 void R_LoadLightGrid( lump_t *l ) {
 	int		i;
 	vec3_t	maxs;
-	int		numGridPoints;
 	world_t	*w;
 	float	*wMins, *wMaxs;
 
@@ -2660,21 +2659,23 @@ void R_LoadLightGrid( lump_t *l ) {
 		w->lightGridBounds[i] = (maxs[i] - w->lightGridOrigin[i])/w->lightGridSize[i] + 1;
 	}
 
-	numGridPoints = w->lightGridBounds[0] * w->lightGridBounds[1] * w->lightGridBounds[2];
+	int numGridDataElements = l->filelen / sizeof(*w->lightGridData);
 
-	if ( l->filelen != numGridPoints * 8 ) {
-		ri->Printf( PRINT_WARNING, "WARNING: light grid mismatch\n" );
-		w->lightGridData = NULL;
-		return;
-	}
-
-	w->lightGridData = (byte *)ri->Hunk_Alloc( l->filelen, h_low );
+	w->lightGridData = (mgrid_t *)ri->Hunk_Alloc( l->filelen, h_low );
 	Com_Memcpy( w->lightGridData, (void *)(fileBase + l->fileofs), l->filelen );
 
 	// deal with overbright bits
-	for ( i = 0 ; i < numGridPoints ; i++ ) {
-		R_ColorShiftLightingBytes( &w->lightGridData[i*8], &w->lightGridData[i*8] );
-		R_ColorShiftLightingBytes( &w->lightGridData[i*8+3], &w->lightGridData[i*8+3] );
+	for ( i = 0 ; i < numGridDataElements ; i++ ) 
+	{
+		for(int j = 0; j < MAXLIGHTMAPS; j++)
+		{
+			R_ColorShiftLightingBytes(
+				w->lightGridData[i].ambientLight[j],
+				w->lightGridData[i].ambientLight[j]);
+			R_ColorShiftLightingBytes(
+				w->lightGridData[i].directLight[j],
+				w->lightGridData[i].directLight[j]);
+		}
 	}
 
 	// load hdr lightgrid
@@ -2695,14 +2696,14 @@ void R_LoadLightGrid( lump_t *l ) {
 
 			//ri->Printf(PRINT_ALL, "found!\n");
 
-			if (size != sizeof(float) * 6 * numGridPoints)
+			if (size != sizeof(float) * 6 * numGridDataElements)
 			{
-				ri->Error(ERR_DROP, "Bad size for %s (%i, expected %i)!", filename, size, (int)(sizeof(float)) * 6 * numGridPoints);
+				ri->Error(ERR_DROP, "Bad size for %s (%i, expected %i)!", filename, size, (int)(sizeof(float)) * 6 * numGridDataElements);
 			}
 
 			w->hdrLightGrid = (float *)ri->Hunk_Alloc(size, h_low);
 
-			for (i = 0; i < numGridPoints ; i++)
+			for (i = 0; i < numGridDataElements ; i++)
 			{
 				w->hdrLightGrid[i * 6    ] = hdrLightGrid[i * 6    ] * lightScale;
 				w->hdrLightGrid[i * 6 + 1] = hdrLightGrid[i * 6 + 1] * lightScale;
@@ -2716,6 +2717,29 @@ void R_LoadLightGrid( lump_t *l ) {
 		if (hdrLightGrid)
 			ri->FS_FreeFile(hdrLightGrid);
 	}
+}
+
+/*
+================
+R_LoadLightGridArray
+
+================
+*/
+void R_LoadLightGridArray( lump_t *l ) {
+	world_t	*w;
+
+	w = &s_worldData;
+
+	w->numGridArrayElements = w->lightGridBounds[0] * w->lightGridBounds[1] * w->lightGridBounds[2];
+
+	if ( (unsigned)l->filelen != w->numGridArrayElements * sizeof(*w->lightGridArray) ) {
+		Com_Printf (S_COLOR_YELLOW  "WARNING: light grid array mismatch\n" );
+		w->lightGridData = NULL;
+		return;
+	}
+
+	w->lightGridArray = (unsigned short *)Hunk_Alloc( l->filelen, h_low );
+	memcpy( w->lightGridArray, (void *)(fileBase + l->fileofs), l->filelen );
 }
 
 /*
@@ -3580,11 +3604,10 @@ void RE_LoadWorldMap( const char *name ) {
 	R_LoadSubmodels (&header->lumps[LUMP_MODELS]);
 	R_LoadVisibility( &header->lumps[LUMP_VISIBILITY] );
 	R_LoadLightGrid( &header->lumps[LUMP_LIGHTGRID] );
-
-	// determine vertex light directions
-	R_CalcVertexLightDirs();
+	R_LoadLightGridArray( &header->lumps[LUMP_LIGHTARRAY] );
 
 	// determine which parts of the map are in sunlight
+#if 0
 	if (0)
 	{
 		world_t	*w;
@@ -3771,6 +3794,7 @@ void RE_LoadWorldMap( const char *name ) {
 
 		Z_Free(primaryLightGrid);
 	}
+#endif
 
 	// load cubemaps 
    	if (r_cubeMapping->integer) 
@@ -3799,6 +3823,9 @@ void RE_LoadWorldMap( const char *name ) {
 
 	// only set tr.world now that we know the entire level has loaded properly
 	tr.world = &s_worldData;
+
+	// determine vertex light directions
+	R_CalcVertexLightDirs();
 
 	// make sure the VBO glState entries are safe
 	R_BindNullVBO();

codemp/rd-rend2/tr_init.cpp
@@ -1644,8 +1644,32 @@ qhandle_t stub_RegisterServerSkin( const char *name )
 }
 
 qboolean stub_InitializeWireframeAutomap() { return qtrue; }
-void stub_RE_GetLightStyle (int style, byte *color){}
-void stub_RE_SetLightStyle (int style, int color){}
+
+void RE_GetLightStyle(int style, color4ub_t color)
+{
+	if (style >= MAX_LIGHT_STYLES)
+	{
+	    Com_Error( ERR_FATAL, "RE_GetLightStyle: %d is out of range", (int)style );
+		return;
+	}
+
+	*(int *)color = *(int *)styleColors[style];
+}
+
+void RE_SetLightStyle(int style, int color)
+{
+	if (style >= MAX_LIGHT_STYLES)
+	{
+	    Com_Error( ERR_FATAL, "RE_SetLightStyle: %d is out of range", (int)style );
+		return;
+	}
+
+	if (*(int*)styleColors[style] != color)
+	{
+		*(int *)styleColors[style] = color;
+	}
+}
+
 void stub_RE_GetBModelVerts (int bModel, vec3_t *vec, vec_t *normal) {}
 void stub_RE_WorldEffectCommand ( const char *cmd ){}
 void stub_RE_AddMiniRefEntityToScene ( const miniRefEntity_t *ent ) {}
@@ -1728,8 +1752,8 @@ Q_EXPORT refexport_t* QDECL GetRefAPI ( int apiVersion, refimport_t *rimp ) {
 	re.GetEntityToken = R_GetEntityToken;
 	re.inPVS = R_inPVS;
 
-	re.GetLightStyle = stub_RE_GetLightStyle;
-	re.SetLightStyle = stub_RE_SetLightStyle;
+	re.GetLightStyle = RE_GetLightStyle;
+	re.SetLightStyle = RE_SetLightStyle;
 	re.GetBModelVerts = stub_RE_GetBModelVerts;
 
 	// SetRangedFog

codemp/rd-rend2/tr_light.cpp
@@ -127,11 +127,11 @@ static void R_SetupEntityLightingGrid( trRefEntity_t *ent, world_t *world ) {
 	vec3_t	lightOrigin;
 	int		pos[3];
 	int		i, j;
-	byte	*gridData;
 	float	frac[3];
 	int		gridStep[3];
 	vec3_t	direction;
 	float	totalFactor;
+	unsigned short	*startGridPos;
 
 	if ( ent->e.renderfx & RF_LIGHTING_ORIGIN ) {
 		// seperate lightOrigins are needed so an object that is
@@ -163,40 +163,45 @@ static void R_SetupEntityLightingGrid( trRefEntity_t *ent, world_t *world ) {
 	assert( world->lightGridData ); // NULL with -nolight maps
 
 	// trilerp the light value
-	gridStep[0] = 8;
-	gridStep[1] = 8 * world->lightGridBounds[0];
-	gridStep[2] = 8 * world->lightGridBounds[0] * world->lightGridBounds[1];
-	gridData = world->lightGridData + pos[0] * gridStep[0]
-		+ pos[1] * gridStep[1] + pos[2] * gridStep[2];
+	gridStep[0] = 1;
+	gridStep[1] = 1 * world->lightGridBounds[0];
+	gridStep[2] = 1 * world->lightGridBounds[0] * world->lightGridBounds[1];
+	startGridPos = tr.world->lightGridArray + (pos[0] * gridStep[0] + pos[1] * gridStep[1] + pos[2] * gridStep[2]);
 
 	totalFactor = 0;
 	for ( i = 0 ; i < 8 ; i++ ) {
 		float	factor;
-		byte	*data;
+		mgrid_t	*data;
+		unsigned short	*gridPos;
 		int		lat, lng;
 		vec3_t	normal;
-		qboolean ignore;
+
 		#if idppc
 		float d0, d1, d2, d3, d4, d5;
 		#endif
+
 		factor = 1.0;
-		data = gridData;
-		ignore = qfalse;
+		gridPos = startGridPos;
+
 		for ( j = 0 ; j < 3 ; j++ ) {
 			if ( i & (1<<j) ) {
-				if ((pos[j] + 1) >= world->lightGridBounds[j] - 1)
-				{
-					ignore = qtrue; // ignore values outside lightgrid
-				}
 				factor *= frac[j];
-				data += gridStep[j];
+				gridPos += gridStep[j];
 			} else {
-				factor *= (1.0f - frac[j]);
+				factor *= (1.0 - frac[j]);
 			}
 		}
 
-		if ( ignore )
+		if (gridPos >= tr.world->lightGridArray + tr.world->numGridArrayElements)
+		{//we've gone off the array somehow
 			continue;
+		}
+
+		data = tr.world->lightGridData + *gridPos;
+		if ( data->styles[0] == LS_LSNONE ) 
+		{
+			continue;	// ignore samples in walls
+		}
 
 		if (world->hdrLightGrid)
 		{
@@ -205,12 +210,7 @@ static void R_SetupEntityLightingGrid( trRefEntity_t *ent, world_t *world ) {
 				continue;	// ignore samples in walls
 			}
 		}
-		else
-		{
-			if (!(data[0]+data[1]+data[2]+data[3]+data[4]+data[5]) ) {
-				continue;	// ignore samples in walls
-			}
-		}
+
 		totalFactor += factor;
 		#if idppc
 		d0 = data[0]; d1 = data[1]; d2 = data[2];
@@ -239,17 +239,29 @@ static void R_SetupEntityLightingGrid( trRefEntity_t *ent, world_t *world ) {
 		}
 		else
 		{
-			ent->ambientLight[0] += factor * data[0];
-			ent->ambientLight[1] += factor * data[1];
-			ent->ambientLight[2] += factor * data[2];
+			for(j=0;j<MAXLIGHTMAPS;j++)
+			{
+				if (data->styles[j] != LS_LSNONE)
+				{
+					const byte	style= data->styles[j];
 
-			ent->directedLight[0] += factor * data[3];
-			ent->directedLight[1] += factor * data[4];
-			ent->directedLight[2] += factor * data[5];
+					ent->ambientLight[0] += factor * data->ambientLight[j][0] * styleColors[style][0] / 255.0f;
+					ent->ambientLight[1] += factor * data->ambientLight[j][1] * styleColors[style][1] / 255.0f;
+					ent->ambientLight[2] += factor * data->ambientLight[j][2] * styleColors[style][2] / 255.0f;
+
+					ent->directedLight[0] += factor * data->directLight[j][0] * styleColors[style][0] / 255.0f;
+					ent->directedLight[1] += factor * data->directLight[j][1] * styleColors[style][1] / 255.0f;
+					ent->directedLight[2] += factor * data->directLight[j][2] * styleColors[style][2] / 255.0f;
+				}
+				else
+				{
+					break;
+				}
+			}
 		}
 		#endif
-		lat = data[7];
-		lng = data[6];
+		lat = data->latLong[1];
+		lng = data->latLong[0];
 		lat *= (FUNCTABLE_SIZE/256);
 		lng *= (FUNCTABLE_SIZE/256);
 

codemp/rd-rend2/tr_local.h
@@ -438,6 +438,12 @@ typedef enum {
 						// in addition to alpha test
 	SS_BANNER,
 
+	SS_INSIDE,			// inside body parts (i.e. heart)
+	SS_MID_INSIDE,
+	SS_MIDDLE,
+	SS_MID_OUTSIDE,
+	SS_OUTSIDE,			// outside body parts (i.e. ribs)
+
 	SS_FOG,
 
 	SS_UNDERWATER,		// for items that should be drawn in front of the water plane
@@ -1589,6 +1595,15 @@ typedef struct {
 	int			numSurfaces;
 } bmodel_t;
 
+typedef struct 
+{
+	byte		ambientLight[MAXLIGHTMAPS][3];
+	byte		directLight[MAXLIGHTMAPS][3];
+	byte		styles[MAXLIGHTMAPS];
+	byte		latLong[2];
+//	byte		pad[2];								// to align to a cache line
+} mgrid_t;
+
 typedef struct {
 	char		name[MAX_QPATH];		// ie: maps/tim_dm2.bsp
 	char		baseName[MAX_QPATH];	// ie: tim_dm2
@@ -1633,8 +1648,15 @@ typedef struct {
 	vec3_t		lightGridSize;
 	vec3_t		lightGridInverseSize;
 	int			lightGridBounds[3];
-	byte		*lightGridData;
 	float		*hdrLightGrid;
+	int			lightGridOffsets[8];
+
+	vec3_t		lightGridStep;
+
+	mgrid_t		*lightGridData;
+	word		*lightGridArray;
+	int			numGridArrayElements;
+
 
 
 	int			numClusters;
@@ -2571,6 +2593,7 @@ typedef struct shaderCommands_s
 } shaderCommands_t;
 
 extern	shaderCommands_t	tess;
+extern	color4ub_t	styleColors[MAX_LIGHT_STYLES];
 
 void RB_BeginSurface(shader_t *shader, int fogNum, int cubemapIndex );
 void RB_EndSurface(void);

codemp/rd-rend2/tr_main.cpp
@@ -1853,6 +1853,16 @@ void R_AddDrawSurf( surfaceType_t *surface, shader_t *shader,
 					int cubemap) {
 	int			index;
 
+	if (tr.refdef.rdflags & RDF_NOFOG)
+	{
+		fogIndex = 0;
+	}
+
+	if ( (shader->surfaceFlags & SURF_FORCESIGHT) /*&& !(tr.refdef.rdflags & RDF_ForceSightOn)*/ )
+	{	//if shader is only seen with ForceSight and we don't have ForceSight on, then don't draw
+		return;
+	}
+
 	// instead of checking for overflow, we just mask the index
 	// so it wraps around
 	index = tr.refdef.numDrawSurfs & DRAWSURF_MASK;

codemp/rd-rend2/tr_shade.cpp
@@ -33,6 +33,8 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
   This file deals with applying shaders to surface data in the tess struct.
 */
 
+color4ub_t	styleColors[MAX_LIGHT_STYLES];
+
 
 /*
 ==================
@@ -1018,6 +1020,18 @@ static unsigned int RB_CalcShaderVertexAttribs( shaderCommands_t *input )
 	return vertexAttribs;
 }
 
+static void ForceAlpha (unsigned char *dstColors, int forceEntAlpha)
+{
+	int	i;
+
+	dstColors += 3;
+
+	for ( i = 0; i < tess.numVertexes; i++, dstColors += 4 )
+	{
+		*dstColors = forceEntAlpha;
+	}
+}
+
 static void RB_IterateStagesGeneric( shaderCommands_t *input )
 {
 	int stage;
@@ -1038,6 +1052,8 @@ static void RB_IterateStagesGeneric( shaderCommands_t *input )
 		shaderProgram_t *sp;
 		vec4_t texMatrix;
 		vec4_t texOffTurb;
+		int stateBits;
+		int forceRGBGen = 0;
 
 		if ( !pStage )
 		{
@@ -1140,7 +1156,23 @@ static void RB_IterateStagesGeneric( shaderCommands_t *input )
 			GLSL_SetUniformFloat(sp, UNIFORM_FOGEYET, eyeT);
 		}
 
-		GL_State( pStage->stateBits );
+		stateBits = pStage->stateBits;
+
+		if ( backEnd.currentEntity )
+		{
+			assert(backEnd.currentEntity->e.renderfx >= 0);
+
+			if ( backEnd.currentEntity->e.renderfx & RF_DISINTEGRATE1 )
+			{
+				// we want to be able to rip a hole in the thing being disintegrated, and by doing the depth-testing it avoids some kinds of artefacts, but will probably introduce others?
+				stateBits = GLS_SRCBLEND_SRC_ALPHA | GLS_DSTBLEND_ONE_MINUS_SRC_ALPHA | GLS_DEPTHMASK_TRUE | GLS_ATEST_GE_C0;
+			}
+
+			if ( backEnd.currentEntity->e.renderfx & RF_RGB_TINT )
+			{//want to use RGBGen from ent
+				forceRGBGen = CGEN_ENTITY;
+			}
+		}
 
 		{
 			vec4_t baseColor;
@@ -1339,6 +1371,24 @@ static void RB_IterateStagesGeneric( shaderCommands_t *input )
 		//
 		if (!(tr.viewParms.flags & VPF_NOCUBEMAPS) && input->cubemapIndex && r_cubeMapping->integer)
 			GL_BindToTMU( tr.cubemaps[input->cubemapIndex - 1], TB_CUBEMAP);
+
+		if (backEnd.currentEntity && (backEnd.currentEntity->e.renderfx & RF_FORCE_ENT_ALPHA))
+		{
+			ForceAlpha((unsigned char *) tess.svars.colors, backEnd.currentEntity->e.shaderRGBA[3]);
+			if (backEnd.currentEntity->e.renderfx & RF_ALPHA_DEPTH)
+			{ //depth write, so faces through the model will be stomped over by nearer ones. this works because
+				//we draw RF_FORCE_ENT_ALPHA stuff after everything else, including standard alpha surfs.
+				GL_State(GLS_SRCBLEND_SRC_ALPHA | GLS_DSTBLEND_ONE_MINUS_SRC_ALPHA | GLS_DEPTHMASK_TRUE);
+			}
+			else
+			{
+				GL_State(GLS_SRCBLEND_SRC_ALPHA | GLS_DSTBLEND_ONE_MINUS_SRC_ALPHA);
+			}
+		}
+		else
+		{
+			GL_State( stateBits );
+		}
 
 		//
 		// draw

codemp/rd-rend2/tr_shader.cpp
@@ -1582,15 +1582,15 @@ void ParseSort( const char **text ) {
 	} else if ( !Q_stricmp( token, "underwater" ) ) {
 		shader.sort = SS_UNDERWATER;
 	} else if ( !Q_stricmp( token, "inside" ) ) {
-		//shader.sort = SS_INSIDE;
+		shader.sort = SS_INSIDE;
 	} else if ( !Q_stricmp( token, "mid_inside" ) ) {
-		//shader.sort = SS_MID_INSIDE;
+		shader.sort = SS_MID_INSIDE;
 	} else if ( !Q_stricmp( token, "middle" ) ) {
-		//shader.sort = SS_MIDDLE;
+		shader.sort = SS_MIDDLE;
 	} else if ( !Q_stricmp( token, "mid_outside" ) ) {
-		//shader.sort = SS_MID_OUTSIDE;
+		shader.sort = SS_MID_OUTSIDE;
 	} else if ( !Q_stricmp( token, "outside" ) ) {
-		//shader.sort = SS_OUTSIDE;
+		shader.sort = SS_OUTSIDE;
 	}	
 	else {
 		shader.sort = atof( token );
@@ -3122,7 +3122,15 @@ static shader_t *FinishShader( void ) {
 				if ( pStage->stateBits & GLS_DEPTHMASK_TRUE ) {
 					shader.sort = SS_SEE_THROUGH;
 				} else {
-					shader.sort = SS_BLEND0;
+					if (( blendSrcBits == GLS_SRCBLEND_ONE ) && ( blendDstBits == GLS_DSTBLEND_ONE ))
+					{
+						// GL_ONE GL_ONE needs to come a bit later
+						shader.sort = SS_BLEND1;
+					}
+					else
+					{
+						shader.sort = SS_BLEND0;
+					}
 				}
 			}
 		}

commit c59e705013003dcd2f182cf5316a853a3729f176

Rend2: Fix objects rendering as pure black
This turned out to be a bug I introduced back in 8325f6c when I was
fixing a lot of the initial rendering bugs. There was some issues with
the depth range not being restored after they had been changed by the
RenderDrawSurfLists function, which would affect subsequent renders.

codemp/rd-rend2/tr_backend.cpp
@@ -687,11 +687,9 @@ static void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 							GL_SetProjectionMatrix( backEnd.viewParms.projectionMatrix );
 						}
 
-						if (!sunflare)
-							qglDepthRange (0, 1);
+						if( !sunflare )
+							qglDepthRange( 0.0f, 1.0f );
 
-						depth[0] = 0;
-						depth[1] = 1;
 						break;
 
 					case 1:
@@ -704,12 +702,9 @@ static void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 							GL_SetProjectionMatrix( temp.projectionMatrix );
 						}
 
- 						if(!oldDepthRange)
-						{
-							depth[0] = 0;
-							depth[1] = 0.3f;
- 							qglDepthRange (depth[0], depth[1]);
-	 					}
+ 						if ( !oldDepthRange )
+ 							qglDepthRange( 0.0f, 0.3f );
+
 						break;
 
 					case 2:
@@ -722,12 +717,9 @@ static void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 							GL_SetProjectionMatrix( temp.projectionMatrix );
 						}
 
- 						if(!oldDepthRange)
-						{
-							depth[0] = 0.0f;
-							depth[1] = 0.0f;
- 							qglDepthRange (depth[0], depth[1]);
-	 					}
+ 						if ( !oldDepthRange )
+ 							qglDepthRange( 0.0f, 0.0f );
+
 						break;
 				}
 
@@ -758,7 +750,8 @@ static void RB_RenderDrawSurfList( drawSurf_t *drawSurfs, int numDrawSurfs ) {
 
 	GL_SetModelviewMatrix( backEnd.viewParms.world.modelMatrix );
 
-	//qglDepthRange (0, 1);
+	// Restore depth range for subsequent rendering
+	qglDepthRange( 0.0f, 1.0f );
 }
 
 