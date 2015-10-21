a4b864e5508102acb41f6baa8cf884b8330f5dd3
Zyk <zyk6870@gmail.com>
2015-10-21 12:27:01 -0200

    Increased ammo spent by Blaster Pistol alt fire.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/bg_weapons.c
+++ codemp/game/bg_weapons.c
@@ -105,7 +105,7 @@ weaponData_t weaponData[WP_NUM_WEAPONS] =
 		1,//2,						//	int		energyPerShot;		// Amount of energy used per shot
 		800,//400,					//	int		fireTime;			// Amount of time between firings
 		8192,					//	int		range;				// Range of weapon
-		1,//2,						//	int		altEnergyPerShot;	// Amount of energy used for alt-fire
+		2,//2,						//	int		altEnergyPerShot;	// Amount of energy used for alt-fire
 		800,//400,					//	int		altFireTime;		// Amount of time between alt-firings
 		8192,					//	int		altRange;			// Range of alt-fire
 		0,						//	int		chargeSubTime;		// ms interval for subtracting ammo during charge
