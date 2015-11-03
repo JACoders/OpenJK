9d126be54df3ee062e3bc638829f2db869acef9b
Willi Schinmeyer <willi@schinmeyer.de>
2015-11-03 10:33:29 +0100

    Added cstring_view literal _v
    i.e. "foo"_v creates a cstring_view of length 3 containing "foo"

--- shared/qcommon/safe/gsl.h
+++ shared/qcommon/safe/gsl.h
@@ -2,3 +2,9 @@
 
 // central point of include to simplify possible future swap for Microsoft's implementation
 #include <gsl/gsl-lite.h>
+
+/** gsl::cstring_view from string literal (without null-termination) */
+inline gsl::cstring_view operator"" _v( const char* str, std::size_t length )
+{
+	return{ str, str + length };
+}
