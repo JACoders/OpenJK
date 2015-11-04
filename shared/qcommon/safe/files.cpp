5682f58d558a361e7ca80d7792ad1efea1f99268
Willi Schinmeyer <willi@schinmeyer.de>
2015-11-04 21:15:30 +0100

    fixed missing centity_t define in jagame safe/file.cpp

--- shared/qcommon/safe/files.cpp
+++ shared/qcommon/safe/files.cpp
@@ -1,6 +1,7 @@
 #include "files.h"
 
 #if defined( SP_GAME )
+# define GAME_INCLUDE // so centity_t is defined in g_shared.h (alternatively include g_public.h?)
 # include "game/g_shared.h" // gi
 #else
 # include "qcommon/qcommon.h"
