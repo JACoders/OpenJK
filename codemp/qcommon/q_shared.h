40023bf9d81c67d44efb4dea69a84ddf5d2c0e5b
Willi Schinmeyer <willi@schinmeyer.de>
2015-11-06 00:06:23 +0100

    renamed _UI define to UI_BUILD
    Identifiers starting with _ and an uppercase letter are prohibited; in particular this clashes with the Mac STL

--- codemp/qcommon/q_shared.h
+++ codemp/qcommon/q_shared.h
@@ -1157,7 +1157,7 @@ qboolean Info_Validate( const char *s );
 qboolean Info_NextPair( const char **s, char *key, char *value );
 
 // this is only here so the functions in q_shared.c and bg_*.c can link
-#if defined( _GAME ) || defined( _CGAME ) || defined( _UI )
+#if defined( _GAME ) || defined( _CGAME ) || defined( UI_BUILD )
 	void (*Com_Error)( int level, const char *error, ... );
 	void (*Com_Printf)( const char *msg, ... );
 #else
