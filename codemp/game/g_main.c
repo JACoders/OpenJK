bd56c5af97b63548294a7bc92721f879660278e9
videoP <videoprofess@gmail.com>
2015-12-10 19:49:42 -0500

    revert

--- codemp/game/g_main.c
+++ codemp/game/g_main.c
@@ -451,7 +451,7 @@ void G_InitGame( int levelTime, int randomSeed, int restart ) {
 
 	if (level.gametype == GT_SIEGE)
 	{ //just get these configstrings registered now...
-		/*
+		
 		while (i < MAX_CUSTOM_SIEGE_SOUNDS)
 		{
 			if (!bg_customSiegeSoundNames[i])
@@ -461,7 +461,8 @@ void G_InitGame( int levelTime, int randomSeed, int restart ) {
 			G_SoundIndex((char *)bg_customSiegeSoundNames[i]);
 			i++;
 		}
-		*/
+		
+		/*
 		for (i = 0; i < MAX_CUSTOM_SIEGE_SOUNDS; i++)
 		{
 			if (!bg_customSiegeSoundNames[i])
@@ -469,9 +470,10 @@ void G_InitGame( int levelTime, int randomSeed, int restart ) {
 				break;
 			}
 			G_SoundIndex((char *)bg_customSiegeSoundNames[i]);
-		}
+		*/
 	}
 
+	/*
 	if (g_allowVGS.integer) {
 		for (i = 0; i < MAX_CUSTOM_VGS_SOUNDS; i++)
 		{
@@ -482,6 +484,7 @@ void G_InitGame( int levelTime, int randomSeed, int restart ) {
 			G_SoundIndex((char *)bg_customVGSSoundNames[i]);
 		}
 	}
+	*/
 
 	if ( level.gametype == GT_JEDIMASTER ) { 
 		gentity_t *ent = NULL;
