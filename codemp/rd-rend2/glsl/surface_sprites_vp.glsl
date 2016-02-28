JACoders/OpenJK

commit 48e2170c3a93912e83a0437ad3b3ad02a4212378

Rend2: shader permutations for surface sprites
This commit also turns off back face culling for surface sprites.

codemp/rd-rend2/glsl/surface_sprites_vp.glsl
@@ -49,14 +49,14 @@ void main()
 		vec2(0.0, 0.0)
 	);
 
-	vec3 preOffset = offsets[gl_VertexID];
+	vec3 offset = offsets[gl_VertexID];
 
 #if defined(FACE_CAMERA)
 	vec2 toCamera = normalize(V.xy);
-	vec2 perp = vec2(toCamera.y, -toCamera.x);
-	vec3 offset = vec3(perp*preOffset.x, preOffset.z);
+	offset.xy *= vec2(toCamera.y, -toCamera.x);
 #else
-	vec3 offset = preOffset;
+	// Make this sprite face in some direction
+	//offset.xy *= attr_Normal.xy;
 #endif
 
 	vec4 worldPos = vec4(attr_Position + offset, 1.0);

codemp/rd-rend2/tr_bsp.cpp
@@ -3402,6 +3402,7 @@ static uint32_t UpdateHash( const char *text, uint32_t hash )
 
 static void R_GenerateSurfaceSprites(
 		const srfBspSurface_t *bspSurf,
+		const shader_t *shader,
 		const shaderStage_t *stage,
 		srfSprites_t *out)
 {
@@ -3488,7 +3489,10 @@ static void R_GenerateSurfaceSprites(
 			sizeof(sprite_t) * sprites.size(), VBO_USAGE_STATIC);
 	out->shader = R_CreateShaderFromTextureBundle(va("*ss_%08x\n", hash),
 			bundle, stage->stateBits);
-	out->shader->stages[0]->glslShaderGroup = &tr.spriteShader;
+	// FIXME: Need a better way to handle this.
+	out->shader->cullType = shader->cullType;
+	out->shader->stages[0]->glslShaderGroup = tr.spriteShader;
+	out->shader->stages[0]->alphaTestCmp = stage->alphaTestCmp;
 	out->numAttributes = 2;
 	out->attributes = (vertexAttribute_t *)ri->Hunk_Alloc(
 			sizeof(vertexAttribute_t) * 2, h_low);
@@ -3552,7 +3556,7 @@ static void R_GenerateSurfaceSprites( const world_t *world )
 					}
 
 					srfSprites_t *sprite = surf->surfaceSprites + surfaceSpriteNum;
-					R_GenerateSurfaceSprites(bspSurf, stage, sprite);
+					R_GenerateSurfaceSprites(bspSurf, shader, stage, sprite);
 					++surfaceSpriteNum;
 				}
 				break;

codemp/rd-rend2/glsl/surface_sprites_fp.glsl
@@ -27,10 +27,10 @@ void main()
 	out_Color = texture(u_DiffuseMap, var_TexCoords);
 	out_Color.a *= var_Alpha*(1.0 - alphaTestValue) + alphaTestValue;
 
-//#if defined(ALPHA_TEST)
+#if defined(ALPHA_TEST)
 	if ( out_Color.a < alphaTestValue )
 		discard;
-//#endif
+#endif
 
 	out_Glow = vec4(0.0);
 }

codemp/rd-rend2/tr_glsl.cpp
@@ -1611,12 +1611,25 @@ int GLSL_BeginLoadGPUShaders(void)
 	}
 
 	attribs = ATTR_POSITION | ATTR_NORMAL;
-	extradefines[0] = '\0';
-	if (!GLSL_BeginLoadGPUShader(&tr.spriteShader, "surface_sprites", attribs,
-				qtrue, extradefines, fallbackShader_surface_sprites_vp,
-				fallbackShader_surface_sprites_fp))
+	for ( int i = 0; i < SSDEF_COUNT; ++i )
 	{
-		ri->Error(ERR_FATAL, "Could not load surface sprites shader!");
+		extradefines[0] = '\0';
+
+		if ( i & SSDEF_FACE_CAMERA )
+			Q_strcat(extradefines, sizeof(extradefines),
+					"#define FACE_CAMERA\n");
+
+		if ( i & SSDEF_ALPHA_TEST )
+			Q_strcat(extradefines, sizeof(extradefines),
+					"#define ALPHA_TEST\n");
+
+		if (!GLSL_BeginLoadGPUShader(tr.spriteShader + i, "surface_sprites",
+					attribs, qtrue, extradefines,
+					fallbackShader_surface_sprites_vp,
+					fallbackShader_surface_sprites_fp))
+		{
+			ri->Error(ERR_FATAL, "Could not load surface sprites shader!");
+		}
 	}
 
 	return startTime;
@@ -1914,12 +1927,16 @@ void GLSL_EndLoadGPUShaders ( int startTime )
 
 	numEtcShaders++;
 
-	if (!GLSL_EndLoadGPUShader(&tr.spriteShader))
-		ri->Error(ERR_FATAL, "Could not compile surface sprites shader!");
+	for ( int i = 0; i < SSDEF_COUNT; ++i )
+	{
+		shaderProgram_t *program = tr.spriteShader + i;
+		if (!GLSL_EndLoadGPUShader(program))
+			ri->Error(ERR_FATAL, "Could not compile surface sprites shader!");
 
-	GLSL_InitUniforms(&tr.spriteShader);
-	GLSL_FinishGPUShader(&tr.spriteShader);
-	numEtcShaders++;
+		GLSL_InitUniforms(program);
+		GLSL_FinishGPUShader(program);
+		numEtcShaders++;
+	}
 
 #if 0
 	attribs = ATTR_POSITION | ATTR_TEXCOORD0;

codemp/rd-rend2/tr_local.h
@@ -1079,6 +1079,16 @@ enum
 	LIGHTDEF_COUNT               		= 0x0800
 };
 
+// Surface sprite shader flags
+enum
+{
+	SSDEF_FACE_CAMERA					= 0x01,
+	SSDEF_ALPHA_TEST					= 0x02,
+
+	SSDEF_ALL							= 0x03,
+	SSDEF_COUNT							= SSDEF_ALL + 1
+};
+
 enum
 {
 	GLSL_INT,
@@ -2194,7 +2204,7 @@ typedef struct trGlobals_s {
 	shaderProgram_t glowCompositeShader;
 	shaderProgram_t dglowDownsample;
 	shaderProgram_t dglowUpsample;
-	shaderProgram_t spriteShader;
+	shaderProgram_t spriteShader[SSDEF_COUNT];
 
 	// -----------------------------------------
 

codemp/rd-rend2/tr_surface.cpp
@@ -2124,14 +2124,23 @@ static void RB_SurfaceSprites( srfSprites_t *surf )
 	// TODO: Check which pass (z-prepass/shadow/forward) we're rendering for?
 	shader_t *shader = surf->shader;
 	shaderStage_t *firstStage = shader->stages[0];
-	shaderProgram_t *program = firstStage->glslShaderGroup;
+	shaderProgram_t *programGroup = firstStage->glslShaderGroup;
+	const surfaceSprite_t *ss = surf->sprite;
 
+	uint32_t shaderFlags = 0;
+	if ( firstStage->alphaTestCmp != ATEST_CMP_NONE )
+		shaderFlags |= SSDEF_ALPHA_TEST;
+
+	if ( ss->type == SURFSPRITE_ORIENTED )
+		shaderFlags |= SSDEF_FACE_CAMERA;
+
+	shaderProgram_t *program = programGroup + shaderFlags;
 	assert(program->uniformBlocks & (1 << UNIFORM_BLOCK_SURFACESPRITE));
 
-	const surfaceSprite_t *ss = surf->sprite;
 	SurfaceSpriteBlock data = {};
 	data.width = ss->width;
-	data.height = ss->height;
+	data.height = (ss->facing == SURFSPRITE_FACING_DOWN)
+					? -ss->height : ss->height;
 	data.fadeStartDistance = ss->fadeDist;
 	data.fadeEndDistance = ss->fadeMax;
 	data.fadeScale = ss->fadeScale;
@@ -2140,6 +2149,7 @@ static void RB_SurfaceSprites( srfSprites_t *surf )
 
 	GLSL_BindProgram(program);
 	GL_State(firstStage->stateBits);
+	GL_Cull(CT_TWO_SIDED);
 	GL_VertexAttribPointers(surf->numAttributes, surf->attributes);
 	R_BindAnimatedImageToTMU(&firstStage->bundle[0], TB_DIFFUSEMAP);
 	RB_UpdateUniformBlock(UNIFORM_BLOCK_SURFACESPRITE, &data);

codemp/rd-rend2/tr_vbo.cpp
@@ -638,6 +638,7 @@ void RB_UpdateUniformBlock(uniformBlock_t block, void *data)
 	qglBindBufferRange(GL_UNIFORM_BUFFER, blockInfo->slot,
 			thisFrame->ubo, thisFrame->uboWriteOffset, blockInfo->size);
 
+	// FIXME: Use actual ubo alignment
 	size_t alignedBlockSize = (blockInfo->size + 255) & ~255;
 	thisFrame->uboWriteOffset += alignedBlockSize;
 }

commit 341396f300e6e478fdff550ec5b763e8a6a537ec

Rend2: Randomize direction of surface sprites
Also fix shader code to rotate the surface sprites, broken in 48e2170c3.

codemp/rd-rend2/glsl/surface_sprites_vp.glsl
@@ -53,10 +53,10 @@ void main()
 
 #if defined(FACE_CAMERA)
 	vec2 toCamera = normalize(V.xy);
-	offset.xy *= vec2(toCamera.y, -toCamera.x);
+	offset.xy = offset.x*vec2(toCamera.y, -toCamera.x);
 #else
 	// Make this sprite face in some direction
-	//offset.xy *= attr_Normal.xy;
+	offset.xy = offset.x*attr_Normal.xy;
 #endif
 
 	vec4 worldPos = vec4(attr_Position + offset, 1.0);

codemp/rd-rend2/tr_bsp.cpp
@@ -3450,8 +3450,8 @@ static void R_GenerateSurfaceSprites(
 		{
 			for ( float b = 0.0f, bend = (1.0f - a); b < bend; b += step )
 			{
-				float x = random()*step + a;
-				float y = random()*step + b;
+				float x = flrand(0.0f, 1.0f)*step + a;
+				float y = flrand(0.0f, 1.0f)*step + b;
 				float z = 1.0f - x - y;
 
 				// Ensure we're inside the triangle bounds.
@@ -3465,10 +3465,11 @@ static void R_GenerateSurfaceSprites(
 				VectorMA(sprite.position, y, p1, sprite.position);
 				VectorMA(sprite.position, z, p2, sprite.position);
 
-				VectorMA(sprite.normal, x, n0, sprite.normal);
-				VectorMA(sprite.normal, y, n1, sprite.normal);
-				VectorMA(sprite.normal, z, n2, sprite.normal);
-				VectorNormalize(sprite.normal);
+				float nx = flrand(-1.0f, 1.0f);
+				float ny = std::sqrtf(1.0f - nx*nx);
+				ny *= irand(0, 1) ? -1 : 1;
+
+				VectorSet(sprite.normal, nx, ny, 0.0f);
 
 				// We have 4 copies for each corner of the quad
 				sprites.push_back(sprite);
@@ -3500,7 +3501,7 @@ static void R_GenerateSurfaceSprites(
 			sizeof(vertexAttribute_t) * 2, h_low);
 
 	out->attributes[0].vbo = out->vbo;
-	out->attributes[0].index = 0;
+	out->attributes[0].index = ATTR_INDEX_POSITION;
 	out->attributes[0].numComponents = 3;
 	out->attributes[0].integerAttribute = qfalse;
 	out->attributes[0].type = GL_FLOAT;
@@ -3510,12 +3511,13 @@ static void R_GenerateSurfaceSprites(
 	out->attributes[0].stepRate = 1;
 
 	out->attributes[1].vbo = out->vbo;
-	out->attributes[1].index = 1;
+	out->attributes[1].index = ATTR_INDEX_NORMAL;
 	out->attributes[1].numComponents = 3;
 	out->attributes[1].integerAttribute = qfalse;
 	out->attributes[1].type = GL_FLOAT;
 	out->attributes[1].normalize = GL_FALSE;
 	out->attributes[1].stride = sizeof(sprite_t);
+	out->attributes[1].offset = offsetof(sprite_t, normal);
 	out->attributes[1].stepRate = 1;
 }
 