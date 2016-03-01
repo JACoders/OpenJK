2c93aa6bdee09b87e3df254dbe4e272335bf192e
eternalcodes <dan2077x@gmail.com>
2016-02-29 18:12:17 -0600

    Revert "Update cl_console.cpp"
    This reverts commit 17e6faae00172f41fe676534e0149552e125daf2.

--- codemp/client/cl_console.cpp
+++ codemp/client/cl_console.cpp
@@ -470,8 +470,6 @@ void CL_ConsolePrint( const char *txt) {
 	}
 
 	color = ColorIndex(COLOR_WHITE);
-	
-	// Xethioz: Buggy fix for timestamp in console
 
 	/*if (timeString) {
 		time_t rawtime;
@@ -483,10 +481,8 @@ void CL_ConsolePrint( const char *txt) {
 		txt = timedText;
 	}*/
 
-	qboolean last = qfalse;
 	while ( (c = (unsigned char) *txt) != 0 ) {
 		if ( Q_IsColorString( (unsigned char*) txt ) ) {
-			last = qfalse;
 			color = ColorIndex( *(txt+1) );
 			txt += 2;
 			continue;
@@ -495,7 +491,6 @@ void CL_ConsolePrint( const char *txt) {
 		// count word length
 		for (l=0 ; l< con.linewidth ; l++) {
 			if ( txt[l] <= ' ') {
-				timeString = qfalse;
 				break;
 			}
 		}
@@ -512,7 +507,6 @@ void CL_ConsolePrint( const char *txt) {
 		{
 		case '\n':
 			Con_Linefeed (skipnotify);
-			last = qtrue;
 			break;
 		case '\r':
 			con.x = 0;
@@ -526,7 +520,6 @@ void CL_ConsolePrint( const char *txt) {
 			}
 			break;
 		}
-		timeString = last;
 	}
 
 
