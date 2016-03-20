2b949f7a0ee9b9876f72fe23f56c81840d4a42c5
Alex Nordlund <deep.alexander@gmail.com>
2016-03-20 11:31:10 +0100

    Adds BUILDDIR variable (with default) to buildbot's windows make.bat

--- tools/builds/make.bat
+++ tools/builds/make.bat
@@ -1,1 +1,5 @@
-cmake -G "Visual Studio 12" -D CMAKE_INSTALL_PREFIX=install -D CMAKE_BUILD_TYPE=Release "--build build" .
+set BUILDDIR=%1
+IF [%BUILDDIR%] == [] (
+    set BUILDDIR=.
+)
+cmake -G "Visual Studio 12" -D CMAKE_INSTALL_PREFIX=install -D CMAKE_BUILD_TYPE=Release %BUILDDIR%
