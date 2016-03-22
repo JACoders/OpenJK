55d9184d89f7a5d260af5a486f02fe1afd3f6c16
Ensiform <ensiform@gmail.com>
2016-03-21 21:15:32 -0500

    MP: Add WIP support for CVar and Cmd Descriptions.
    Also adds the following commands: `help`, `cvarAdd`, `cvarSub`, `cvarMult`, `cvarDiv`, and `cvarMod`.
    
    Refs #747

--- codemp/qcommon/net_ip.cpp
+++ codemp/qcommon/net_ip.cpp
@@ -975,7 +975,7 @@ void NET_Init( void ) {
 
 	NET_Config( qtrue );
 
-	Cmd_AddCommand ("net_restart", NET_Restart_f );
+	Cmd_AddCommand ("net_restart", NET_Restart_f, "Restart the networking sub-system" );
 }
 
 /*
