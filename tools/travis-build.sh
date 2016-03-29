9c999911b629fd5bfd3105cfe25cfa01e96ffd2c
eternalcodes <dan2077x@gmail.com>
2016-03-29 18:28:25 -0500

    Add releases for Linux and MacOSX

--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -21,13 +21,27 @@ case "${host}" in
 	(i?86-linux-gnu)
 		set -- \
 			-D CMAKE_TOOLCHAIN_FILE=$(pwd)/CMakeModules/Toolchains/linux-i686.cmake \
+			-D BuildMPGame=OFF \
+			-D BuildMPDed=OFF \
+			-D BuildMPCGame=OFF \
 			"$@"
 		;;
 	(macosx-universal-clang)
+		set -- \
+			-D BuildMPGame=OFF \
+			-D BuildMPDed=OFF \
+			-D BuildMPCGame=OFF \
+			"$@"
 		;;
 	(native)
+		if [ -n "${deploy}" ]; then
+			set -- \
+				-D BuildMPGame=OFF \
+				-D BuildMPDed=OFF \
+				-D BuildMPCGame=OFF \
+				"$@"
+		fi
 		;;
-
 	(*)
 		set +x
 		echo "Error: don't know how to cross-compile for ${host} host"
@@ -42,4 +56,43 @@ set -- -D CMAKE_BUILD_TYPE="$flavour" "$@"
 	"$@" .. )
 make -C build
 make -C build install DESTDIR=$(pwd)/build/DESTDIR
-( cd $(pwd)/build/DESTDIR && find . -ls )
+
+if [ x"${host}" = xi686-linux-gnu ]; then
+	arch="i686"
+else
+	arch="x86_64"
+fi
+
+case "${host}" in
+	(macosx-universal-clang)
+		( cd $(pwd)/assets && \
+			zip -r japro3ui-assets.pk3 * && \
+			mv japro3ui-assets.pk3 ../build/DESTDIR/prefix/JediAcademy/openjk.x86_64.app/Contents/MacOS/japro/ && \
+			cd ../build/DESTDIR/prefix/JediAcademy/openjk.x86_64.app/Contents/MacOS/japro/ && \
+			wget http://upsgaming.com/files/japro3.pk3 && \
+			cd ../../../ && \
+			tar czvf eternaljk-macos-"${arch}".tar.gz * && \
+			mv eternaljk-macos-"${arch}".tar.gz /Users/travis/build/eternalcodes/EternalJK/ && \
+			cd ../../../ && \
+			find . -ls )
+		;;
+	(i?86-linux-gnu|native)
+		if [ -n "${deploy}" ]; then
+			( cd $(pwd)/assets && \
+				zip -r japro3ui-assets.pk3 * && \
+				mv japro3ui-assets.pk3 ../build/DESTDIR/prefix/JediAcademy/japro/ && \
+				cd ../build/DESTDIR/prefix/JediAcademy/japro && \
+				wget http://upsgaming.com/files/japro3.pk3 && \
+				cd ../ && \
+				tar czvf eternaljk-linux-"${arch}".tar.gz * && \
+				mv eternaljk-linux-"${arch}".tar.gz /home/travis/build/eternalcodes/EternalJK/ && \
+				cd ../../ && \
+				find . -ls )
+		else
+			( cd $(pwd)/build/DESTDIR && find . -ls )
+		fi
+		;;
+	(*)
+		( cd $(pwd)/build/DESTDIR && find . -ls )
+		;;
+esac
