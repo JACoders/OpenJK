b983d45878baa8fe2bc86ec8bd64606e5929e57e
Zyk <zyk6870@gmail.com>
2016-05-16 20:54:07 -0300

    Changed version to 3.34. Fixed bug in /magic command.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -22,7 +22,7 @@ extern vec3_t gPainPoint;
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"Zyk OpenJK Mod v3.33"
+#define	GAMEVERSION	"Zyk OpenJK Mod v3.34"
 
 #define SECURITY_LOG "security.log"
 
