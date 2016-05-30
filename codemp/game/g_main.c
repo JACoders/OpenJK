dceb31e65a0514f57619ff09f71258dfd6f67a0c
Zyk <zyk6870@gmail.com>
2016-05-30 02:22:02 -0300

    Decreased Ultra Flame duration a little.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_main.c
+++ codemp/game/g_main.c
@@ -5030,7 +5030,7 @@ void ultra_flame(gentity_t *ent, int distance, int damage)
 
 		if (zyk_special_power_can_hit_target(ent, player_ent, i, 0, distance, qfalse, &targets_hit) == qtrue)
 		{
-			zyk_quest_effect_spawn(ent, player_ent, "zyk_quest_effect_flame", "4", "env/flame_jet", 800, damage, 35, 50000);
+			zyk_quest_effect_spawn(ent, player_ent, "zyk_quest_effect_flame", "4", "env/flame_jet", 800, damage, 35, 40000);
 		}
 	}
 }
