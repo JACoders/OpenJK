d74e78e6cdd03e189fe2fe757fd23360db3a1b84
eternalcodes <dan2077x@gmail.com>
2016-07-31 17:56:13 -0500

    fs_basegame changed to eternaljk

--- codemp/qcommon/files.cpp
+++ codemp/qcommon/files.cpp
@@ -3347,7 +3347,7 @@ void FS_Startup( const char *gameName ) {
 	fs_copyfiles = Cvar_Get( "fs_copyfiles", "0", CVAR_INIT );
 	fs_cdpath = Cvar_Get ("fs_cdpath", "", CVAR_INIT|CVAR_PROTECTED, "(Read Only) Location for development files" );
 	fs_basepath = Cvar_Get ("fs_basepath", Sys_DefaultInstallPath(), CVAR_INIT|CVAR_PROTECTED, "(Read Only) Location for game files" );
-	fs_basegame = Cvar_Get ("fs_basegame", "japro", CVAR_INIT );
+	fs_basegame = Cvar_Get ("fs_basegame", "eternaljk", CVAR_INIT );
 	homePath = Sys_DefaultHomePath();
 	if (!homePath || !homePath[0]) {
 		homePath = fs_basepath->string;
