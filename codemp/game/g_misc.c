7c53c3cdb6095f173f9d0d046829c923eaf2796d
Zyk <zyk6870@gmail.com>
2016-09-11 23:40:15 -0300

    Decreased Super Beam damage a little.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_misc.c
+++ codemp/game/g_misc.c
@@ -2839,7 +2839,7 @@ void fx_runner_think( gentity_t *ent )
 		if (player_ent && player_ent->client && user_ent && user_ent->client && user_ent != player_ent &&
 			zyk_is_ally(user_ent, player_ent) == qfalse)
 		{ // zyk: if the enemy is hit by the super beam, damage him
-			G_Damage(player_ent, user_ent, user_ent, NULL, player_ent->client->ps.origin, 50, DAMAGE_NO_PROTECTION, MOD_UNKNOWN);
+			G_Damage(player_ent, user_ent, user_ent, NULL, player_ent->client->ps.origin, 40, DAMAGE_NO_PROTECTION, MOD_UNKNOWN);
 		}
 
 		ent->nextthink = level.time + 100;
