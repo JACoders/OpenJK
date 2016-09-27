0653c64d7c4f303fd582c830cd310883b587a6d5
Circa <circacitadel@live.com>
2016-09-27 18:48:19 -0500

    Update g_local.h

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -44,7 +44,7 @@ extern vec3_t gPainPoint;
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-#define	GAMEVERSION	"OpenJK"
+#define	GAMEVERSION	"basejka"
 
 #define SECURITY_LOG "security.log"
 
