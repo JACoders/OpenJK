8f526492c23a9e2420a8686db0eae953e13ce652
Zyk <zyk6870@gmail.com>
2016-12-03 12:12:14 -0200

    Reduced Sleeping Flowers stun time and Poison Mushrooms damage a little.
    Signed-off-by: Zyk <zyk6870@gmail.com>

--- codemp/game/g_cmds.c
+++ codemp/game/g_cmds.c
@@ -4143,7 +4143,7 @@ qboolean TryGrapple(gentity_t *ent)
 					else if (use_this_power == 6 && zyk_enable_sleeping_flowers.integer == 1 && ent->client->pers.magic_power >= (int)ceil((zyk_sleeping_flowers_mp_cost.integer * universe_mp_cost_factor)))
 					{
 						ent->client->ps.powerups[PW_FORCE_ENLIGHTENED_LIGHT] = level.time + 1000;
-						sleeping_flowers(ent,3000,350);
+						sleeping_flowers(ent,2500,350);
 						ent->client->pers.magic_power -= (int)ceil((zyk_sleeping_flowers_mp_cost.integer * universe_mp_cost_factor));
 						if (ent->client->pers.rpg_class == 8)
 							ent->client->pers.quest_power_usage_timer = level.time + (zyk_sleeping_flowers_cooldown.integer * ((4.0 - ent->client->pers.skill_levels[55])/4.0)) + (500 * ent->client->pers.skill_levels[55]);
--- codemp/game/g_main.c
+++ codemp/game/g_main.c
@@ -5854,7 +5854,7 @@ void quest_power_events(gentity_t *ent)
 
 						// zyk: Universe Power
 						if (ent->client->pers.quest_power_status & (1 << 13))
-							G_Damage(ent,poison_mushrooms_user,poison_mushrooms_user,NULL,NULL,38,0,MOD_UNKNOWN);
+							G_Damage(ent,poison_mushrooms_user,poison_mushrooms_user,NULL,NULL,35,0,MOD_UNKNOWN);
 						else
 							G_Damage(ent,poison_mushrooms_user,poison_mushrooms_user,NULL,NULL,32,0,MOD_UNKNOWN);
 					}
