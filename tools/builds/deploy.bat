b823aa7662c49832eb6ff689e153beeb8a29e2fd
Xycaleth <alex@acslo.com>
2017-03-12 21:01:30 +0000

    Maybe run rsync in cygwin bash?

--- tools/builds/deploy.bat
+++ tools/builds/deploy.bat
@@ -1,5 +1,4 @@
-echo "%APPVEYOR_FORCED_BUILD%"
 if "%APPVEYOR_FORCED_BUILD%"=="True" (
 	C:\cygwin\setup-x86.exe -qnNdO -R C:/cygwin -s http://cygwin.mirror.constant.com -l C:/cygwin/var/cache/setup -P rsync
-	rsync -avz --progress -e 'ssh -p 29022 ' --protocol=29 openjk-windows*.zip ojkwinbuilder@upload.openjk.org:/home/ojkwinbuilder/builds/
+	C:\cygwin\bin\bash -lc rsync -avz --progress -e 'ssh -p 29022' --protocol=29 openjk-windows*.zip ojkwinbuilder@upload.openjk.org:/home/ojkwinbuilder/builds/
 )
