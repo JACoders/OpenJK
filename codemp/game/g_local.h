5fd1a578c7d01719eb53166f1d2f4c1160d7bacf
Circa <circacitadel@live.com>
2017-04-07 10:34:17 -0400

    Version 1.03
    Only adds cheats to rcon

--- codemp/game/g_local.h
+++ codemp/game/g_local.h
@@ -44,7 +44,7 @@ extern vec3_t gPainPoint;
 //==================================================================
 
 // the "gameversion" client command will print this plus compile date
-// OpenJK-Alt by Circa		Version 1.02
+// OpenJK-Alt by Circa		Version 1.03
 #define	GAMEVERSION	"basejka"
 
 #define SECURITY_LOG "security.log"
