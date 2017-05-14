c9e1de88ab965adfb547aec2ae0d7a804ba7b4f8
Ensiform <ensiform@gmail.com>
2017-05-13 22:21:02 -0500

    Shared: Fix Sys_QueEvent overflow on mouse events
    This was a bug that is introduced in all known tech3 engines using SDL even the original SDL unix ports by TTimo.  But is mostly only noticeable on newer machines and with high DPI mice.
    
    What this patch does is consolodates mouse events into one event when sent together to avoid the spam.  Coincidentally we can probably even reduce the queue size because I doubt it would ever reach half of what it is at now.
    
    Fix comes from https://github.com/ec-/Quake3e eugene (ec-). Multiple commits across code/qcommon/common.c so I'm not going to list the exact hashes.

--- shared/sys/sys_public.h
+++ shared/sys/sys_public.h
@@ -67,7 +67,8 @@ typedef enum {
 	SE_CHAR,	// evValue is an ascii char
 	SE_MOUSE,	// evValue and evValue2 are reletive signed x / y moves
 	SE_JOYSTICK_AXIS,	// evValue is an axis number and evValue2 is the current state (-127 to 127)
-	SE_CONSOLE	// evPtr is a char*
+	SE_CONSOLE,	// evPtr is a char*
+	SE_MAX
 } sysEventType_t;
 
 typedef struct sysEvent_s {
