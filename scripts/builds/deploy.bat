1bd54091bdac93a505afc10bd43c14781161739b
Xycaleth <alex@acslo.com>
2017-06-23 20:12:01 +0100

    chmod the .zip file to 0644 on rsync

--- scripts/builds/deploy.bat
+++ scripts/builds/deploy.bat
@@ -6,5 +6,5 @@ set ZIP_FILE=%1
 
 if "%APPVEYOR_FORCED_BUILD%"=="True" (
 	%CYGWIN_ROOT%\setup-x86.exe -qnNdO -R %CYGWIN_ROOT% -s http://cygwin.mirror.constant.com -l %CYGWIN_ROOT%/var/cache/setup -P rsync
-	%CYGWIN_ROOT%\bin\bash -lc 'rsync -avz --progress -e "ssh -p 29022 -o StrictHostKeyChecking=no -i %WIN32_ROOT%/users/appveyor/.ssh/id_rsa" --protocol=29 %WIN32_ROOT%/projects/openjk/%ZIP_FILE% "%DEPLOY_LOCATION%"'
+	%CYGWIN_ROOT%\bin\bash -lc 'rsync -avz --progress -e "ssh -p 29022 -o StrictHostKeyChecking=no -i %WIN32_ROOT%/users/appveyor/.ssh/id_rsa -p --chmod=0644" --protocol=29 %WIN32_ROOT%/projects/openjk/%ZIP_FILE% "%DEPLOY_LOCATION%"'
 )
