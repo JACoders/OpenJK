a6b6bf0ea6bbe8168567a83628b4928858aa03fb
Xycaleth <alex@acslo.com>
2017-11-09 16:50:46 +0800

    Use Nvidia and AMD device when available
    On laptops which have dual GPUs, it's desirable to use the dedicated
    graphics device instead of the integrated one.

--- codemp/rd-rend2/tr_init.cpp
+++ codemp/rd-rend2/tr_init.cpp
@@ -32,6 +32,13 @@ static size_t FRAME_UNIFORM_BUFFER_SIZE = 8*1024*1024;
 static size_t FRAME_VERTEX_BUFFER_SIZE = 8*1024*1024;
 static size_t FRAME_INDEX_BUFFER_SIZE = 2*1024*1024;
 
+#if defined(_WIN32)
+extern "C" {
+    _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
+
 glconfig_t  glConfig;
 glconfigExt_t glConfigExt;
 glRefConfig_t glRefConfig;
