1514979f7b74298fd83f7cdacca1a59507628d2e
Alex Lo <xycaleth@gmail.com>
2018-05-06 22:23:11 +0100

    Remove tools which were moved to OpenJK-Tools

--- tools/Assimilate/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	Assimilate.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
