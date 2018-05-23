325c80240e0fad9c8ea13ba74a74bba9b57c2a99
Bucky21659 <mjmiller98@gmail.com>
2018-05-23 15:52:13 -0600

    Remove s_khz cvar because apparently it doesn't do anything, only show field in UI on jamp.exe (v1.01)

--- codemp/ui/ui_main.c
+++ codemp/ui/ui_main.c
@@ -5674,10 +5674,14 @@ static void UI_StartSkirmish(qboolean next) {
 
 static void UI_Update(const char *name) {
 	int	val = trap->Cvar_VariableValue(name);
+	char ver[64];
+
+	trap->Cvar_VariableStringBuffer("version", ver, sizeof(ver));
 
 	if (Q_stricmp(name, "s_khz") == 0)
 	{
-		trap->Cmd_ExecuteText( EXEC_APPEND, "snd_restart\n" );
+		if (!Q_stricmpn(ver, "JAmp: v1.0.1.0 win-x86 Oct 24 2003", 35))
+			trap->Cmd_ExecuteText( EXEC_APPEND, "snd_restart\n" );
 		return;
 	}
 
