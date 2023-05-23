722066ef62c4e7ab5544b44c6439b82d1ddac88a
SomaZ <17459161+SomaZ@users.noreply.github.com>
2023-05-23 15:28:26 +0200

    Remove PR noise 3

--- codemp/rd-common/tr_font.cpp
+++ codemp/rd-common/tr_font.cpp
@@ -1793,15 +1793,6 @@ void R_ReloadFonts_f(void)
 	}
 }
 
-void R_InitFreeType(void) {
-#ifdef BUILD_FREETYPE
-	if (FT_Init_FreeType( &ftLibrary )) {
-		ri.Printf(PRINT_WARNING, "R_InitFreeType: Unable to initialize FreeType.\n");
-	}
-#endif
-	// I DONT DO NOTHING
-}
-
 
 void R_DoneFreeType(void) {
 #ifdef BUILD_FREETYPE
