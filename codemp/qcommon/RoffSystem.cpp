677d7b1e61e3d7917214026e459212d82d5ecad3
Daggolin <Daggolin@Gmx.de>
2023-09-16 18:33:37 +0200

    [MP] Fix ROFF system using incorrect time value.
    The ROFF system previously used engine time instead of module time when setting the time values for trajectories of shared entities. This caused issues when the server module time has been reset on map change, because the game module would misinterpret the trajectories.

--- codemp/qcommon/RoffSystem.cpp
+++ codemp/qcommon/RoffSystem.cpp
@@ -647,7 +647,7 @@ qboolean CROFFSystem::Play( int entID, int id, qboolean doTranslation, qboolean 
 
 	roffing_ent->mEntID			= entID;
 	roffing_ent->mROFFID		= id;
-	roffing_ent->mNextROFFTime	= svs.time;
+	roffing_ent->mNextROFFTime	= sv.time;
 	roffing_ent->mROFFFrame		= 0;
 	roffing_ent->mKill			= qfalse;
 	roffing_ent->mSignal		= qtrue; // TODO: hook up the real signal code
@@ -864,7 +864,7 @@ qboolean CROFFSystem::ApplyROFF( SROFFEntity *roff_ent, CROFFSystem::CROFF *roff
 	float			*origin = NULL, *angle = NULL;
 
 
-	if ( svs.time < roff_ent->mNextROFFTime )
+	if ( sv.time < roff_ent->mNextROFFTime )
 	{ // Not time to roff yet
 		return qtrue;
 	}
@@ -900,8 +900,8 @@ qboolean CROFFSystem::ApplyROFF( SROFFEntity *roff_ent, CROFFSystem::CROFF *roff
 
 	if ( roff_ent->mROFFFrame >= roff->mROFFEntries )
 	{ // we are done roffing, so stop moving and flag this ent to be removed
-		SetLerp( originTrajectory, TR_STATIONARY, origin, NULL, svs.time, roff->mLerp );
-		SetLerp( angleTrajectory, TR_STATIONARY, angle, NULL, svs.time, roff->mLerp );
+		SetLerp( originTrajectory, TR_STATIONARY, origin, NULL, sv.time, roff->mLerp );
+		SetLerp( angleTrajectory, TR_STATIONARY, angle, NULL, sv.time, roff->mLerp );
 		if (!roff_ent->mIsClient)
 		{
 			ent->r.mIsRoffing = qfalse;
@@ -922,11 +922,11 @@ qboolean CROFFSystem::ApplyROFF( SROFFEntity *roff_ent, CROFFSystem::CROFF *roff
 	}
 
 	// Set up our origin interpolation
-	SetLerp( originTrajectory, TR_LINEAR, origin, result, svs.time, roff->mLerp );
+	SetLerp( originTrajectory, TR_LINEAR, origin, result, sv.time, roff->mLerp );
 
 	// Set up our angle interpolation
 	SetLerp( angleTrajectory, TR_LINEAR, angle,
-				roff->mMoveRotateList[roff_ent->mROFFFrame].mRotateOffset, svs.time, roff->mLerp );
+				roff->mMoveRotateList[roff_ent->mROFFFrame].mRotateOffset, sv.time, roff->mLerp );
 
 	if (roff->mMoveRotateList[roff_ent->mROFFFrame].mStartNote >= 0)
 	{
@@ -940,7 +940,7 @@ qboolean CROFFSystem::ApplyROFF( SROFFEntity *roff_ent, CROFFSystem::CROFF *roff
 
 	// Advance ROFF frames and lock to a 10hz cycle
 	roff_ent->mROFFFrame++;
-	roff_ent->mNextROFFTime = svs.time + roff->mFrameTime;
+	roff_ent->mNextROFFTime = sv.time + roff->mFrameTime;
 
 	//rww - npcs need to know when they're getting roff'd
 	if ( !roff_ent->mIsClient )
@@ -1044,8 +1044,8 @@ qboolean CROFFSystem::ClearLerp( SROFFEntity *roff_ent )
 		angle = ent->r.currentAngles;
 	}
 
-	SetLerp( originTrajectory, TR_STATIONARY, origin, NULL, svs.time, ROFF_SAMPLE_RATE );
-	SetLerp( angleTrajectory, TR_STATIONARY, angle, NULL, svs.time, ROFF_SAMPLE_RATE );
+	SetLerp( originTrajectory, TR_STATIONARY, origin, NULL, sv.time, ROFF_SAMPLE_RATE );
+	SetLerp( angleTrajectory, TR_STATIONARY, angle, NULL, sv.time, ROFF_SAMPLE_RATE );
 
 	return qtrue;
 }
