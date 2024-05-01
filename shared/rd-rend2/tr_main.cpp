9ff929c44bfb5fa4ca1702f38b85dd4a23488b8e
SomaZ <17459161+SomaZ@users.noreply.github.com>
2024-05-01 14:06:18 +0200

    [rend2] Don't add portal views when there's no world to render

--- shared/rd-rend2/tr_main.cpp
+++ shared/rd-rend2/tr_main.cpp
@@ -2582,7 +2582,7 @@ void R_SetupViewParms(const trRefdef_t *refdef)
 
 qboolean R_AddPortalView(const trRefdef_t *refdef)
 {
-	if (!tr.world)
+	if (!tr.world || refdef->rdflags & RDF_NOWORLDMODEL)
 		return qfalse;
 
 	for (int i = 0; i < tr.world->numWorldSurfaces; i++)
