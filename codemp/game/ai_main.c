79b767d8e13fa9f276b2da1f1d00cd4e13f1b683
Daniel Piers <danielpiers@gmail.com>
2025-11-30 15:17:26 -0800

    [MP] Fix array bounds error in bot enemy scanning
    Fix off-by-one error in ScanForEnemies() where the loop condition
    'while (i <= MAX_CLIENTS)' allowed accessing g_entities[MAX_CLIENTS],
    which is out of bounds. Valid client indices are 0 to MAX_CLIENTS-1.
    
    This could cause memory corruption or crashes when bots scan for enemies.

--- codemp/game/ai_main.c
+++ codemp/game/ai_main.c
@@ -2195,7 +2195,7 @@ int ScanForEnemies(bot_state_t *bs)
 		}
 	}
 
-	while (i <= MAX_CLIENTS)
+	while (i < MAX_CLIENTS)
 	{
 		if (i != bs->client && g_entities[i].client && !OnSameTeam(&g_entities[bs->client], &g_entities[i]) && PassStandardEnemyChecks(bs, &g_entities[i]) && BotPVSCheck(g_entities[i].client->ps.origin, bs->eye) && PassLovedOneCheck(bs, &g_entities[i]))
 		{
